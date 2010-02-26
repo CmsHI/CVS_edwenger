a72292149aa2b8a3805e52c86e4f6a0052729689
edwenger <>
2010-02-26 11:44:38 +0000

    new beamspot and run/lumi hists

--- Skims/macros/analyzeTracks.C
+++ Skims/macros/analyzeTracks.C
@@ -13,9 +13,11 @@
 #include <TFile.h>
 #include <TROOT.h>
 #include <TSystem.h>
+#include <TString.h>
 
 #include "DataFormats/FWLite/interface/Handle.h"
 #include "DataFormats/FWLite/interface/Event.h"
+#include "DataFormats/FWLite/interface/ChainEvent.h"
 
 #if !defined(__CINT__) && !defined(__MAKECINT__)
 
@@ -43,27 +45,37 @@ void analyzeTracks(){
   const unsigned nHitsCut = 1; // at least this many hits on track
 
   //----- histograms -----
+  TH2D *hRunLumi    = new TH2D("hRunLumi","Event information; run number; luminosity block",500,123549.5,124049.5,200,-0.5,199.5);
   TH1D *hL1TechBits = new TH1D("hL1TechBits","L1 technical trigger bits before mask",64,-0.5,63.5);
-  TH2D *hHPFracNtrk = new TH2D("hHPFracNtrk","High purity fraction vs. # of tracks; number of tracks; highPurity fraction",25,0,500,25,0,1);
-  TH2D *hHfTowers   = new TH2D("hHfTowers","Number of HF tower above threshold; positive side; negative side",20,-0.5,19.5,20,-0.5,19.5);
+  TH2D *hHPFracNtrk = new TH2D("hHPFracNtrk","High purity fraction vs. # of tracks; number of tracks; highPurity fraction",50,0,500,50,0,1);
+  TH2D *hHfTowers   = new TH2D("hHfTowers","Number of HF tower above threshold; positive side; negative side",80,-0.5,39.5,80,-0.5,39.5);
 
   TH1D *hVtxSize    = new TH1D("hVtxSize","number of reconstructed pixel vertices",10,-0.5,9.5);
   TH1D *hVtxTrks    = new TH1D("hVtxTrks","number of tracks used to fit pixel vertex",50,-0.5,49.5);
   TH1D *hVtxZ       = new TH1D("hVtxZ","z position of best reconstructed pixel vertex", 80,-20,20);
-  
+  TH2D *hBeamXRun   = new TH2D("hBeamXRun","x position of beamspot; run number",500,123549.5,124049.5,80,-2,2);
+  TH2D *hBeamYRun   = new TH2D("hBeamYRun","y position of beamspot; run number",500,123549.5,124049.5,80,-2,2);
+  TH2D *hBeamZRun   = new TH2D("hBeamZRun","z position of beamspot; run number",500,123549.5,124049.5,80,-2,2);
+
   TH1D *hTrkQual    = new TH1D("hTrkQual","track quality", 20, -0.5, 19.5);
-  TH1D *hTrkDxyBeam = new TH1D("hTrkDxyBeam","track dxy from beamspot; dxy [cm]", 20, -2.0, 2.0);
-  TH1D *hTrkDzVtx   = new TH1D("hTrkDzVtx","track dz from vertex; dz [cm]", 20, -2.0, 2.0);
-  TH2D *hTrkPtErrNhits = new TH2D("hTrkPtErrNhits","track relative pt-error vs. nhits; number of valid hits on track; #sigma(p_{T})/p_{T}",30,-0.5,29.5,20,0.0,0.5);
+  TH1D *hTrkDxyBeam = new TH1D("hTrkDxyBeam","track dxy from beamspot; dxy [cm]", 80, -2.0, 2.0);
+  TH1D *hTrkDzVtx   = new TH1D("hTrkDzVtx","track dz from vertex; dz [cm]", 80, -2.0, 2.0);
+  TH2D *hTrkPtErrNhits = new TH2D("hTrkPtErrNhits","track relative pt-error vs. nhits; number of valid hits on track; #sigma(p_{T})/p_{T}",30,-0.5,29.5,40,0.0,0.5);
   TH1D *hTrkNhits   = new TH1D("hTrkNhits", "number of valid hits on track", 30,-0.5,29.5);
   TH1D *hTrkPt      = new TH1D("hTrkPt","track p_{T}; p_{T} [GeV/c]", 80, 0.0, 20.0);
-  TH1D *hTrkEta     = new TH1D("hTrkEta","track #eta; #eta", 30, -3.0, 3.0);
-  TH1D *hTrkPhi     = new TH1D("hTrkPhi","track #phi; #phi [radians]", 28, -3.5, 3.5);
+  TH1D *hTrkEta     = new TH1D("hTrkEta","track #eta; #eta", 60, -3.0, 3.0);
+  TH1D *hTrkPhi     = new TH1D("hTrkPhi","track #phi; #phi [radians]", 56, -3.5, 3.5);
   
   //----- input files (900 GeV data) -----
   vector<string> fileNames;
-  string fileDir = "../python";
-  fileNames.push_back(fileDir + "/trkAnaSkimAOD.root");
+  //string fileDir = "rfio:/castor/cern.ch/user/e/edwenger/trkAnaSkim/MB_BC09-Feb9ReReco_v2_pptrkana_skim/v4";
+  string fileDir = "/d101/edwenger/data/v4";
+  cout << "directory: '" << fileDir << "'" << endl;
+  for(int ifile=1; ifile<=31; ifile++) {
+    TString name = Form("trkAnaSkimAOD_%d.root",ifile);
+    cout << "  adding file: " << name.Data() << endl;
+    fileNames.push_back(fileDir + "/" + name.Data());
+  }
   fwlite::ChainEvent event(fileNames);
 
   //----- loop over events -----
@@ -71,7 +83,13 @@ void analyzeTracks(){
   for(event.toBegin(); !event.atEnd(); ++event, ++iEvent){
 
     //if( iEvent == 1000 ) break;
-    if( iEvent % 1000 == 0 ) cout<<"Processing event : "<<iEvent<<endl;
+    if( iEvent % 1000 == 0 ) cout << "Processing " << iEvent<< "th event: "
+				  << "run " << event.id().run() 
+				  << ", lumi " << event.luminosityBlock() 
+				  << ", evt " << event.id().event() << endl;
+
+    // fill event info
+    hRunLumi->Fill(event.id().run(),event.luminosityBlock());
 
     // select on L1 trigger bits
     fwlite::Handle<L1GlobalTriggerReadoutRecord> gt;
@@ -123,9 +141,12 @@ void analyzeTracks(){
     hVtxTrks->Fill(maxtracks);
     hVtxZ->Fill(bestvz);
 
-    // get beamspot if there are any tracks in the event
+    // get beamspot
     fwlite::Handle<reco::BeamSpot> beamspot;
-    if(tracks->size()) beamspot.getByLabel(event, "offlineBeamSpot");
+    beamspot.getByLabel(event, "offlineBeamSpot");
+    hBeamXRun->Fill(event.id().run(),beamspot->x0());
+    hBeamYRun->Fill(event.id().run(),beamspot->y0());
+    hBeamZRun->Fill(event.id().run(),beamspot->z0());
 
     //----- loop over tracks -----
     for(unsigned it=0; it<tracks->size(); ++it){
@@ -171,6 +192,7 @@ void analyzeTracks(){
   // write to output file
   TFile outFile("trackHists.root", "recreate" );
   outFile.cd(); outFile.mkdir("evt"); outFile.cd("evt");
+  hRunLumi->Write();
   hL1TechBits->Write();
   hHPFracNtrk->Write();
   hHfTowers->Write();
@@ -179,7 +201,10 @@ void analyzeTracks(){
   hVtxSize->Write();
   hVtxTrks->Write();
   hVtxZ->Write();
-  
+  hBeamXRun->Write();
+  hBeamYRun->Write();
+  hBeamZRun->Write();
+
   outFile.cd(); outFile.mkdir("trk"); outFile.cd("trk");
   hTrkQual->Write();
   hTrkDxyBeam->Write();
@@ -194,6 +219,7 @@ void analyzeTracks(){
   outFile.Close();
 
   // free allocated space
+  delete hRunLumi;
   delete hL1TechBits; 
   delete hHPFracNtrk;
   delete hHfTowers;
@@ -201,6 +227,9 @@ void analyzeTracks(){
   delete hVtxSize;
   delete hVtxTrks;
   delete hVtxZ;
+  delete hBeamXRun;
+  delete hBeamYRun;
+  delete hBeamZRun;
 
   delete hTrkQual; 
   delete hTrkDxyBeam;
