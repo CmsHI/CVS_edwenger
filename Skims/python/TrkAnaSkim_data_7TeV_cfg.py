5581543ddf2a673fe6ca39da10ecb0d8f696ae9a
edwenger <>
2010-04-01 14:23:56 +0000

    add complete list of prompt reco files

--- Skims/python/TrkAnaSkim_data_7TeV_cfg.py
+++ Skims/python/TrkAnaSkim_data_7TeV_cfg.py
@@ -16,12 +16,12 @@ process.source = cms.Source("PoolSource",process.PromptReco7TeV)
 
 # =============== Other Statements =====================
 
-process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(100))
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(1000))
 process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
 process.GlobalTag.globaltag = 'GR10_P_V4A::All'
 
 process.configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.3 $'),
+    version = cms.untracked.string('$Revision: 1.4 $'),
     name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/UserCode/edwenger/Skims/python/TrkAnaSkim_data_7TeV_cfg.py,v $'),
     annotation = cms.untracked.string('BPTX_AND + BSC_OR + !BSCHALO')
 )
