5bcb33384cdf096e42ec7b5161f67fa054cc3742
edwenger <>
2010-04-06 09:05:02 +0000

    andre's vertex constraint producer moved to within Skims package

--- Skims/scripts/setup356.sh
+++ Skims/scripts/setup356.sh
@@ -9,11 +9,6 @@ mv UserCode/edwenger .
 # agglomerative vertex
 cvs co UserCode/FerencSiklerVertexing
 
-# vertex constraint modifications from Andre
-cvs co RecoTracker/TrackProducer/test/VertexConstraintProducer.cc
-cvs co UserCode/ASYoon/PPTrackingTools/test/VertexConstraintProducer.cc
-mv UserCode/ASYoon/PPTrackingTools/test/VertexConstraintProducer.cc RecoTracker/TrackProducer/test/
-
 # track fitters recipe from Giuseppe
 cvs co -r V04-00-02-01 TrackingTools/TrackFitters
 cvs update -r CMSSW_3_5_6 TrackingTools/TrackFitters/BuildFile 
