add9e2de0c174510ada3fb9cbd49b193fee453f5
edwenger <>
2010-04-08 19:55:27 +0000

    revert to scheduled paths where extra reco begins with event filter sequence

--- Skims/python/TrkAnaSkim_data_7TeV_cfg.py
+++ Skims/python/TrkAnaSkim_data_7TeV_cfg.py
@@ -21,7 +21,7 @@ process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
 process.GlobalTag.globaltag = 'GR_R_35X_V6::All'
 
 process.configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.13 $'),
+    version = cms.untracked.string('$Revision: 1.14 $'),
     name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/UserCode/edwenger/Skims/python/TrkAnaSkim_data_7TeV_cfg.py,v $'),
     annotation = cms.untracked.string('BPTX_AND + BSC_OR + !BSCHALO')
 )
@@ -39,38 +39,42 @@ process.eventFilter = cms.Sequence(process.minBiasBscFilter *
 
 #process.load("edwenger.Skims.BeamSpot7TeV_cff")     # custom beamspot db source
 process.load("edwenger.Skims.ChargedCandidates_cff") # make charged candidates from selected tracks
-process.load("edwenger.Skims.RootpleProducer_cfi")   # make wei's rootples
 process.load("edwenger.Skims.ExtraVertex_cff")       # agglomerative pixel vertexing
 process.load("edwenger.Skims.TrackRefit_cff")        # refit constrained to primary vertex
-
+process.load("edwenger.Skims.RootpleProducer_cfi")   # make wei's rootples
 
 # rootuple output file
 process.TFileService = cms.Service("TFileService", 
                                    fileName = cms.string('ROOTuple_HighPurity.root')
                                    )
 
+
 process.extraReco = cms.Sequence(#process.offlineBeamspot *
-                                  process.chargedCandidates *
-                                  cms.ignore(process.primaryVertexFilter * process.rootpleProducer) *
-                                  process.extraVertex *
-                                  process.trackRefit)
+                                 process.chargedCandidates *
+                                 process.extraVertex *
+                                 process.trackRefit *
+                                 process.primaryVertexFilter *
+                                 process.rootpleProducer)
 
-# =============== Final Filter Path =====================
+# =============== Final Filter Paths =====================
 
-process.trkAnaSkim_step = cms.Path(process.eventFilter *
-                                   process.extraReco)
+process.eventFilter_step = cms.Path(process.eventFilter)
+process.extraReco_step = cms.Path(process.eventFilter * process.extraReco)
 
 # =============== Output ================================
 
 process.load("edwenger.Skims.analysisSkimContent_cff")
 process.output = cms.OutputModule("PoolOutputModule",
     process.analysisSkimContent,
-    SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring('trkAnaSkim_step')),
+    SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring('eventFilter_step')),
     dataset = cms.untracked.PSet(
       dataTier = cms.untracked.string('AOD'),
       filterName = cms.untracked.string('TrkAnaFilter')),
     fileName = cms.untracked.string('trkAnaSkimAOD.root')
     )
 
-process.outpath = cms.EndPath(process.output)
+process.output_step = cms.EndPath(process.output)
 
+process.schedule = cms.Schedule(process.eventFilter_step,
+                                process.extraReco_step,
+                                process.output_step)
