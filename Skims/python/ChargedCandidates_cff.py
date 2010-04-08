836c4c4dd1ed1fb31a910bce315af77bb001956d
edwenger <>
2010-04-08 09:21:45 +0000

    placeholder for goodTracks

--- Skims/python/ChargedCandidates_cff.py
+++ Skims/python/ChargedCandidates_cff.py
@@ -1,10 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
-# todo: quality selections on general tracks and pass to allTracks.src
+#goodTracks = cms.EDFilter()
 
 allTracks = cms.EDProducer("ConcreteChargedCandidateProducer",
                            src = cms.InputTag("generalTracks"),
+                           #src = cms.InputTag("goodTracks),
                            particleType = cms.string('pi+')
                            )
 
-chargedCandidates = cms.Sequence(allTracks)
+chargedCandidates = cms.Sequence(#goodTracks *
+                                 allTracks)
