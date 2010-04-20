0a68a190d8337bb4735717931b8fd8053bccafd0
edwenger <>
2010-04-20 08:47:24 +0000

    cleanup

--- Skims/python/analysisSkimContent_cff.py
+++ Skims/python/analysisSkimContent_cff.py
@@ -2,6 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 analysisSkimContent = cms.PSet(
     outputCommands = cms.untracked.vstring('drop *',
+                                           
       # event
       'keep *_offlinePrimaryVertices_*_*',
       'keep *_pixelVertices_*_*',
@@ -9,25 +10,27 @@ analysisSkimContent = cms.PSet(
       'keep *_offlineBeamSpot_*_*',
       'keep *_TriggerResults_*_HLT',
       'keep L1GlobalTriggerReadoutRecord_gtDigis_*_*RECO',
-      # jet
-      'keep *_towerMaker_*_*',
-      'keep *_selectedPatJets*_*_*',
-      # tracks
-      'keep recoTracks_generalTracks_*_*RECO',
-      'keep recoTracks_refitTracks_*_*',
-      'keep recoRecoChargedCandidates_allTracks_*_*'  ,                                   
-      'keep recoTracks_refitTracks_*_*',
-      'keep recoTracks_selectFake*_*_*',
-      'keep recoTracks_selectReal*_*_*',
-      #'keep *_trackingParticleRecoTrackAsssociation_*_*',
-      # mc (if present)
+
+      # mc gen info
       'keep *_*GenJet*_*_*',
       'keep reco*_genParticles_*_*',
       'keep GenEventInfoProduct_*_*_*',
-      # clean up unwanted collections
-      'drop *_*BJetTags*_*_*',
-      'drop *_kt*Jet*_*_*',
-      'drop *_sisCone*Jet*_*_*',
-      'drop *_sc*Jet*_*_*'                                     
+                                           
+      # jet
+      'keep *_towerMaker_*_*',
+      'keep *_selectedPatJets*_*_*',
+                                           
+      # tracks
+      'keep recoTracks_generalTracks_*_*RECO',
+      'keep recoTracks_refitTracks_*_*',
+      'keep recoRecoChargedCandidates_allTracks_*_*',                                   
+      'keep recoTracks_refitTracks_*_*',
+      'keep recoTracks_selectFake*_*_*',
+      'keep recoTracks_selectReal*_*_*',
+
+      # sim track matching
+      #'keep *_trackingParticleRecoTrackAsssociation_*_*',
+      #'keep TrackingParticles_mergedtruth_MergedTrackTruth_*'
+                                           
       )
     )
