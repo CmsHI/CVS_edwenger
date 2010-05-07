c4ad933665b132de9252aeccdfa769528902921b
edwenger <>
2010-05-07 08:12:16 +0000

    cleanup

--- Skims/scripts/setup357.sh
+++ Skims/scripts/setup357.sh
@@ -4,25 +4,25 @@ cd $CMSSW_BASE/src
 
 # skiming code
 cvs co UserCode/edwenger/Skims
+
+# PF test
 cvs co UserCode/edwenger/Misc
-mv UserCode/edwenger .
 
 # agglomerative vertex
 cvs co -r V00-00-07 UserCode/FerencSiklerVertexing
 
-# track fitters recipe from Giuseppe
-#cvs co -r CMSSW_3_5_7 TrackingTools/TrackFitters 
-
 # wei's rootuple producers
 cvs co UserCode/davidlw/ROOTupleAnalyzer
-mv UserCode/davidlw .
 
 # fake and real selector, vertex constraint producer
 cvs co UserCode/ASYoon/PPTrackingTools/FakeAndRealTrackSelector
 cvs co UserCode/ASYoon/PPTrackingTools/VertexConstraintProducer
-mv UserCode/ASYoon/PPTrackingTools/ .
 
-# to run MTV with HitPixelLayersTPSelector.cc
+# MTV with HitPixelLayersTPSelector.cc
 cvs co UserCode/CmsHi/TrackAnalysis
-mv UserCode/CmsHi/ .
 
+# Cleanup
+mv UserCode/edwenger .
+mv UserCode/davidlw .
+mv UserCode/ASYoon/PPTrackingTools/ .
+mv UserCode/CmsHi/ .
