2925e6fd5ad2e80c60ec3471b62ad4f78774b0c3
edwenger <>
2010-05-11 11:59:51 +0000

    check out all of the analyzers

--- Skims/scripts/coresetup357.sh
+++ Skims/scripts/coresetup357.sh
@@ -5,6 +5,12 @@ cd $CMSSW_BASE/src
 # skiming code
 cvs co UserCode/edwenger/Skims
 
+# analyzers
+cvs co UserCode/edwenger/EvtSelAnalyzer
+cvs co UserCode/edwenger/VertexAnalyzer
+cvs co UserCode/edwenger/TrackSpectraAnalyzer
+cvs co UserCode/edwenger/TrkEffAnalyzer
+
 # agglomerative vertex
 cvs co -r V00-00-07 UserCode/FerencSiklerVertexing
 
