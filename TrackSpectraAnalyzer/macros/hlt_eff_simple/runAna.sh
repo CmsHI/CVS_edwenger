35816a77bdfbe549177930ea5811987930e23414
frankma <>
2010-05-18 16:42:34 +0000

    run batch

--- TrackSpectraAnalyzer/macros/hlt_eff_simple/runAna.sh
+++ TrackSpectraAnalyzer/macros/hlt_eff_simple/runAna.sh
@@ -1,7 +1,7 @@
 # use histo
 inFile=../process_aod/outputs/trkhists_try9_100k.root
-root -b -l anaHltJetEff_simple.C+\(\"$inFile\",\"plots/fromHist\",true\)
+root -b -q anaHltJetEff_simple.C+\(\"$inFile\",\"plots/fromHist\",true\)
 
 # use nt
 inFile=dcache:///net/hisrv0001/home/edwenger/t2bat/MinimumBias/MB-C10-PR9-TRKANASKIM-v2/38a0275e6a2a62980cf9b1868edbd493/ROOTuple_HighPurity_*.root
-root -b -l anaHltJetEff_simple.C+\(\"$inFile\",\"plots/fromNt\",false\)
+root -b -q anaHltJetEff_simple.C+\(\"$inFile\",\"plots/fromNt\",false\)
