d1bdc10979f3816955fad00090b6ab7f656ae915
frankma <>
2010-05-19 12:43:21 +0000

    Make runnable on histograms ran from nt

--- TrackSpectraAnalyzer/macros/hlt_eff_simple/runAna.sh
+++ TrackSpectraAnalyzer/macros/hlt_eff_simple/runAna.sh
@@ -1,7 +1,20 @@
+#!/bin/bash -
+if [ $# -lt 1 ]; then
+  echo Usage:
+  echo "  $0 <tag>"
+  exit 1
+fi
+
+tag=$1
+
 # use histo
 inFile=../process_aod/outputs/trkhists_try9_100k.root
-root -b -q anaHltJetEff_simple.C+\(\"$inFile\",\"plots/fromHist\",true\)
+root -b -q anaHltJetEff_simple.C+\(\"$inFile\",\"plots/${tag}_fromHist\",true\)
 
 # use nt
 inFile=dcache:///net/hisrv0001/home/edwenger/t2bat/MinimumBias/MB-C10-PR9-TRKANASKIM-v2/38a0275e6a2a62980cf9b1868edbd493/ROOTuple_HighPurity_*.root
-root -b -q anaHltJetEff_simple.C+\(\"$inFile\",\"plots/fromNt\",false\)
+root -b -q anaHltJetEff_simple.C+\(\"$inFile\",\"plots/${tag}_fromNt\",false\)
+
+# use hist from nt
+inFile=plots/${tag}_fromNt/anahlt.root
+root -l anaHltJetEff_simple.C+\(\"$inFile\",\"plots/${tag}_histFromNt\",true,\"\",1\)
