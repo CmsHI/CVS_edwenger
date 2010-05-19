95ae9e1372d4875262053147de7d03e601beade4
frankma <>
2010-05-19 12:43:04 +0000

    add cleanup of jets (need to confirm the jet eta cut hypothesis). Make runnable on histograms ran from nt

--- TrackSpectraAnalyzer/macros/hlt_eff_simple/anaHltJetEff_simple.C
+++ TrackSpectraAnalyzer/macros/hlt_eff_simple/anaHltJetEff_simple.C
@@ -15,7 +15,9 @@ using namespace std;
 
 void anaHltJetEff_simple(const char * inFileName = "../process_aod/outputs/trkhists_try9_100k.root",
     TString outdir="plots/fromHist",
-    Bool_t useHist = true)
+    Bool_t useHist = true,
+    TString histDir = "trackAna/",
+    Int_t reBinFactor = 8)
 {
   CPlot::sOutDir = outdir;
   Float_t histJetEtMax = 300;
@@ -34,14 +36,14 @@ void anaHltJetEff_simple(const char * inFileName = "../process_aod/outputs/trkhi
     hs1D["hJet0Pt_HltJet15U"] = hJet0Pt_HltJet15U;
     hs1D["hJet0Pt_HltJet30U"] = hJet0Pt_HltJet30U;
     hs1D["hJet0Pt_HltJet50U"] = hJet0Pt_HltJet50U;
-    inFile->GetObject("trackAna/hJet0Pt",hs1D["hJet0Pt"]);
-    inFile->GetObject("trackAna/hJet0Pt_HltMB",hs1D["hJet0Pt_HltMB"]);
-    inFile->GetObject("trackAna/hJet0Pt_HltJet6U",hs1D["hJet0Pt_HltJet6U"]);
-    inFile->GetObject("trackAna/hJet0Pt_HltJet15U",hs1D["hJet0Pt_HltJet15U"]);
-    inFile->GetObject("trackAna/hJet0Pt_HltJet30U",hs1D["hJet0Pt_HltJet30U"]);
-    inFile->GetObject("trackAna/hJet0Pt_HltJet50U",hs1D["hJet0Pt_HltJet50U"]);
+    inFile->GetObject(histDir+"hJet0Pt",hs1D["hJet0Pt"]);
+    inFile->GetObject(histDir+"hJet0Pt_HltMB",hs1D["hJet0Pt_HltMB"]);
+    inFile->GetObject(histDir+"hJet0Pt_HltJet6U",hs1D["hJet0Pt_HltJet6U"]);
+    inFile->GetObject(histDir+"hJet0Pt_HltJet15U",hs1D["hJet0Pt_HltJet15U"]);
+    inFile->GetObject(histDir+"hJet0Pt_HltJet30U",hs1D["hJet0Pt_HltJet30U"]);
+    inFile->GetObject(histDir+"hJet0Pt_HltJet50U",hs1D["hJet0Pt_HltJet50U"]);
 
-    Int_t ngroups = 8; // 1-->0.5GeV per bin
+    Int_t ngroups = reBinFactor; // 1-->0.5GeV per bin
     map<TString, TH1F*>::iterator iter;
     for (iter=hs1D.begin(); iter != hs1D.end(); ++iter) {
       // check
@@ -62,10 +64,10 @@ void anaHltJetEff_simple(const char * inFileName = "../process_aod/outputs/trkhi
     cout << "Analysis on " << nt_jet->GetEntries() << " events" << endl;
     nt_jet->Draw("jet>>hJet0Pt","abs(jeta)<3","goff");
     nt_jet->Draw("jet>>hJet0Pt_HltMB","abs(jeta)<3 && mb","goff");
-    nt_jet->Draw("jet>>hJet0Pt_HltJet6U","abs(jeta)<3 && jet6","goff");
-    nt_jet->Draw("jet>>hJet0Pt_HltJet15U","abs(jeta)<3 && jet15","goff");
-    nt_jet->Draw("jet>>hJet0Pt_HltJet30U","abs(jeta)<3 && jet30","goff");
-    nt_jet->Draw("jet>>hJet0Pt_HltJet50U","abs(jeta)<3 && jet50","goff");
+    nt_jet->Draw("jet>>hJet0Pt_HltJet6U","abs(jeta)<3 && jet6 && jet>6","goff");
+    nt_jet->Draw("jet>>hJet0Pt_HltJet15U","abs(jeta)<3 && jet15 && jet>15","goff");
+    nt_jet->Draw("jet>>hJet0Pt_HltJet30U","abs(jeta)<3 && jet30 && jet>30","goff");
+    nt_jet->Draw("jet>>hJet0Pt_HltJet50U","abs(jeta)<3 && jet50 && jet>50","goff");
   }
 
 
@@ -74,8 +76,8 @@ void anaHltJetEff_simple(const char * inFileName = "../process_aod/outputs/trkhi
   CPlot cpJetPt("JetPt","Jet Pt","p_{T}^{corr jet} [GeV/c]","# evt");
   cpJetPt.SetLogy(1);
   cpJetPt.SetXRange(0,histJetEtMax);
-  cpJetPt.AddHist1D(hs1D["hJet0Pt"],"All Jets","",kViolet+2);
-  cpJetPt.AddHist1D(hs1D["hJet0Pt_HltMB"],"HLT_MinBiasBSC","",kBlue);
+  cpJetPt.AddHist1D(hs1D["hJet0Pt"],"BSC_OR+BPTX+HF_Coinc.","",kViolet+2);
+  //cpJetPt.AddHist1D(hs1D["hJet0Pt_HltMB"],"HLT_MinBiasBSC","",kBlue);
   cpJetPt.AddHist1D(hs1D["hJet0Pt_HltJet6U"],"HLT_L1Jet6U","",kAzure+6);
   cpJetPt.AddHist1D(hs1D["hJet0Pt_HltJet15U"],"HLT_Jet15U","",kGreen-3);
   cpJetPt.AddHist1D(hs1D["hJet0Pt_HltJet30U"],"HLT_Jet30U","",kOrange-5);
@@ -107,13 +109,13 @@ void anaHltJetEff_simple(const char * inFileName = "../process_aod/outputs/trkhi
   CPlot cpHltEff("HltEff","Hlt Eff","p_{T}^{corr jet} [GeV/c]","#epsilon_{HLT}");
   cpHltEff.SetXRange(0,histJetEtMax);
   cpHltEff.SetYRange(0,1.1);
-  cpHltEff.AddGraph(gAEs["gHltEff_HltMB"],"HLT_MinBiasBSC","pz",kBlue);
+  //cpHltEff.AddGraph(gAEs["gHltEff_HltMB"],"HLT_MinBiasBSC","pz",kBlue);
   cpHltEff.AddGraph(gAEs["gHltEff_HltJet6U"],"HLT_L1Jet6U","pz",kAzure+6);
   cpHltEff.AddGraph(gAEs["gHltEff_HltJet15U"],"HLT_Jet15U","pz",kGreen-3);
   cpHltEff.AddGraph(gAEs["gHltEff_HltJet30U"],"HLT_Jet30U","pz",kOrange-5);
   cpHltEff.AddGraph(gAEs["gHltEff_HltJet50U"],"HLT_Jet50U","pz",kRed-2);
   cpHltEff.SetLegendHeader("Calojets |#eta|<3");
-  cpHltEff.SetLegend(0.58,0.38,0.91,0.67);
+  cpHltEff.SetLegend(0.58,0.21,0.91,0.50);
   cpHltEff.Draw(cHltEff,true,"gif");
 
   // All done, save hists
