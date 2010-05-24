23d09cff2c0269ead34e91d1392a12fc520c6867
frankma <>
2010-05-24 20:28:59 +0000

    running on the new processed Ed's data skim

--- TrackSpectraAnalyzer/macros/hlt_eff_simple/runAna.sh
+++ TrackSpectraAnalyzer/macros/hlt_eff_simple/runAna.sh
@@ -1,20 +1,14 @@
 #!/bin/bash -
-if [ $# -lt 1 ]; then
-  echo Usage:
-  echo "  $0 <tag>"
-  exit 1
-fi
-
-tag=$1
+tag=V6
 
 # use histo
 #inFile=../process_aod/outputs/trkhists_try9_100k.root
-inFile=$scratch/data/MinimumBias/MB-C10-PR9-TRKANASKIM-v2/all/trkhists_only_all.root
+inFile=$scratch/data/MinimumBias/MB-C10-PR9-TRKANASKIM-v2_job3/all/trkhists_histOnly_all.root
 root -b -q anaHltJetEff_simple.C+\(\"$inFile\",\"plots/${tag}_fromHist\",true\)
 
 # use nt
 #inFile=dcache:///net/hisrv0001/home/edwenger/t2bat/MinimumBias/MB-C10-PR9-TRKANASKIM-v2/38a0275e6a2a62980cf9b1868edbd493/ROOTuple_HighPurity_*.root
-inFile=$scratch/data/MinimumBias/MB-C10-PR9-TRKANASKIM-v2/trkhists_trkAnaSkimAOD_*_1.root
+inFile=$scratch/data/MinimumBias/MB-C10-PR9-TRKANASKIM-v2_job3/trkhists_trkAnaSkimAOD_*_1.root
 root -b -q anaHltJetEff_simple.C+\(\"$inFile\",\"plots/${tag}_fromNt\",false\)
 
 # use hist from nt
