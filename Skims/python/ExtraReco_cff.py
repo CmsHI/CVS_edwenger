56b3cfba71054ffbdc3164cafa4a663368950c63
sungho <>
2010-05-26 16:39:47 +0000

    refit track

--- Skims/python/ExtraReco_cff.py
+++ Skims/python/ExtraReco_cff.py
@@ -5,6 +5,7 @@ from edwenger.Skims.ExtraVertex_cff import *
 from edwenger.Skims.Analyzers_cff import *
 from edwenger.Skims.TrackSelection_cff import *
 from edwenger.Skims.RootpleProducer_cfi import * 
+from edwenger.Skims.TrackRefit_cff import *
 
 extraReco = cms.Sequence(extraVertex *         ## agglomerative pixel vertexing
                          postEvtSelTest *
@@ -15,5 +16,6 @@ extraReco = cms.Sequence(extraVertex *         ## agglomerative pixel vertexing
                          primaryVertexFilter * ## non-fake, ndof>4, abs(z)<15
                          goodVertices *
                          sortedGoodVertices *
+                         trackRefit * 
                          postTrkVtxTest)
 
