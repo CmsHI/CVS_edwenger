25ce399636a1e10111357fc725d32cff3c1626a3
sungho <>
2010-05-27 17:07:36 +0000

    postVtxTest is moved after chargedCandidates

--- Skims/python/ExtraReco_cff.py
+++ Skims/python/ExtraReco_cff.py
@@ -10,8 +10,8 @@ from edwenger.Skims.TrackRefit_cff import *
 extraReco = cms.Sequence(extraVertex *         ## agglomerative pixel vertexing
                          postEvtSelTest *
                          selectedVertex *      ## most-populated (filters)
-                         postVtxTest *
                          chargedCandidates *   ## selected tracks -> charged candidates
+                         postVtxTest *
                          preTrkVtxTest *
                          primaryVertexFilter * ## non-fake, ndof>4, abs(z)<15
                          goodVertices *
