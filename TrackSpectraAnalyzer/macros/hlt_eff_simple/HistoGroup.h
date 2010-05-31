5a3f8256a8eba4a0c64eb6250fee9717790125c9
frankma <>
2010-05-31 22:29:14 +0000

    begin to add spec histograms

--- TrackSpectraAnalyzer/macros/hlt_eff_simple/HistoGroup.h
+++ TrackSpectraAnalyzer/macros/hlt_eff_simple/HistoGroup.h
@@ -10,17 +10,25 @@ class HistoGroup
 {
   public:
     // methods
-    HistoGroup(TString name);
+    HistoGroup(TString name, Int_t n=0, Float_t xmin=0, Float_t xmax=0);
     void Add(TH1F* h1,Float_t sc=1);
+    void Add(TString hname,Float_t sc=1);
 
     // data
     TString name_;
     std::map<TString,TH1F*> hm_;
     std::map<TString,Float_t> scales_;
+    // histo properties
+    Int_t nbins_;
+    Float_t xmin_;
+    Float_t xmax_;
 };
 
-HistoGroup::HistoGroup(TString name) :
-  name_(name)
+HistoGroup::HistoGroup(TString name, Int_t n, Float_t xmin, Float_t xmax) :
+  name_(name),
+  nbins_(n),
+  xmin_(xmin),
+  xmax_(xmax)
 {
 }
 
@@ -28,5 +36,10 @@ void HistoGroup::Add(TH1F* h1,Float_t sc) {
   assert(h1);
   hm_[h1->GetName()] = h1;
   scales_[h1->GetName()] = sc;
+}
+
+void HistoGroup::Add(TString hname,Float_t sc) {
+  TH1F * h1 = new TH1F(hname,"",nbins_,xmin_,xmax_);
+  Add(h1,sc);
 }
 #endif
