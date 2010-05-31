7d4e33e00e2c45e2e2e1b7825d5e364418eef9d9
frankma <>
2010-05-31 10:01:28 +0000

    moved from cleanDuplicates.sh

---
+++ Skims/prod/condor/listDuplicates.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -
+if [ $# -eq 0 ]; then
+  echo Usage
+  echo "  $0 <indir>"
+  exit 1
+fi
+#inDir=/pnfs/cmsaf.mit.edu/t2bat/cms/store/user/edwenger/MinimumBias/MB-C10-PR9-TRKANASKIM-v2/38a0275e6a2a62980cf9b1868edbd493
+inDir=$1
+tag=trkAnaSkimAOD
+
+for file in `find $inDir | grep $tag | sort`; do
+  base=`echo $file | sed 's/_1\.root$//'`
+  #echo base: $base
+  for i in `seq 1 5`; do
+    next=${base}_$((i+1)).root
+    if [ -f $next ]; then
+      #echo next: $next
+      this=${base}_${i}.root
+      echo $this
+    fi
+  done
+done
