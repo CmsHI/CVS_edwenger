427e9bf838a95a77e21692966a8d51a1dd463135
frankma <>
2010-05-31 10:48:33 +0000

    Ed's update that filter all vertices to be good vertices

--- Skims/prod/condor/submitJob.sh
+++ Skims/prod/condor/submitJob.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -
-jobTag=proc0524
+jobTag=proc0527
 #inputTopDir=/pnfs/cmsaf.mit.edu/t2bat/cms/store/user/edwenger/MinimumBias/MB-C10-A20RR-TRKANASKIM-v2/86d28cd0599312fbc0b38fb077d9e1fc
 #output_dir=/net/hibat0003/d00/scratch/frankma/data/MinimumBias/MB-C10-A20RR-TRKANASKIM-v2_${jobTag}
 #inputTopDir=/pnfs/cmsaf.mit.edu/t2bat/cms/store/user/edwenger/MinimumBias/MB-C10-A20RR-TRKANASKIM-v3/86d28cd0599312fbc0b38fb077d9e1fc
