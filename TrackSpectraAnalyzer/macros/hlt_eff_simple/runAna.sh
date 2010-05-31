398278541f4b581aaf3bbd3fa86906c366b5c6a2
frankma <>
2010-05-31 21:25:21 +0000

    test Ed's PR9 v3, he was still using an old version of analyzer with TH1D's --> the histogram use-case doesn't work out-of-box

--- TrackSpectraAnalyzer/macros/hlt_eff_simple/runAna.sh
+++ TrackSpectraAnalyzer/macros/hlt_eff_simple/runAna.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -
-tag=V6
+tag=V0531
 
 # use histo
-#inFile=../process_aod/outputs/trkhists_try9_100k.root
-inFile=$scratch/data/MinimumBias/MB-C10-PR9-TRKANASKIM-v2_job3/all/trkhists_histOnly_all.root
-root -b -q anaHltJetEff_simple.C+\(\"$inFile\",\"plots/${tag}_fromHist\",true\)
+#inFile=$scratch/data/MinimumBias/MB-C10-PR9-TRKANASKIM-v2_job3/all/trkhists_histOnly_all.root
+inFile=$scratch/data/MinimumBias/MB-C10-PR9-TRKANASKIM-v3/all/trkhists_histOnly_all.root
+#root -b -q anaHltJetEff_simple.C+\(\"$inFile\",\"plots/${tag}_fromHist\",true\)
 
 # use nt
-#inFile=dcache:///net/hisrv0001/home/edwenger/t2bat/MinimumBias/MB-C10-PR9-TRKANASKIM-v2/38a0275e6a2a62980cf9b1868edbd493/ROOTuple_HighPurity_*.root
-inFile=$scratch/data/MinimumBias/MB-C10-PR9-TRKANASKIM-v2_job3/trkhists_trkAnaSkimAOD_*_1.root
-root -b -q anaHltJetEff_simple.C+\(\"$inFile\",\"plots/${tag}_fromNt\",false\)
+#inFile=$scratch/data/MinimumBias/MB-C10-PR9-TRKANASKIM-v2_job3/trkhists_trkAnaSkimAOD_*_1.root
+inFile=$scratch/data/MinimumBias/MB-C10-PR9-TRKANASKIM-v3/trkhists_*.root
+#root -b -q anaHltJetEff_simple.C+\(\"$inFile\",\"plots/${tag}_fromNt\",false\)
 
 # use hist from nt
 inFile=plots/${tag}_fromNt/anahlt.root
