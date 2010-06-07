d928638e6463411dbd8ffd6c0357a4012fb8488f
frankma <>
2010-06-07 16:16:08 +0000

    proc0607, excluded trkAnaSkimAOD_2*.root

--- TrackSpectraAnalyzer/macros/runTop.sh
+++ TrackSpectraAnalyzer/macros/runTop.sh
@@ -1,14 +1,16 @@
 #!/bin/bash -
-tag=V0531_v8
+tag=V0607_v1
 
 # run all
-#stag=$tag/all
-stag=$tag/sub2good
-./runAnaAll.sh $scratch'/data/MinimumBias/MB-C10-PR9-TRKANASKIM-v3_proc0531/sub2good/trkhists_trkAnaSkimAOD_*.root' $stag
-exit
+stag=$tag/all
+./runAnaAll.sh $scratch'/data/MinimumBias/MB-C10-PR9-TRKANASKIM-v3_proc0607/trkhists_trkAnaSkimAOD_*.root' $stag
 
 # run sub ranges of files to check results as a function of lumi
 for i in `seq 1 9`; do
   stag=$tag/sub$i
-  ./runAnaAll.sh $scratch'/data/MinimumBias/MB-C10-PR9-TRKANASKIM-v3_proc0531/trkhists_trkAnaSkimAOD_'$i'*.root' $stag
+  ./runAnaAll.sh $scratch'/data/MinimumBias/MB-C10-PR9-TRKANASKIM-v3_proc0607/trkhists_trkAnaSkimAOD_'$i'*.root' $stag
 done
+
+# 2* treat separately
+stag=$tag/sub2
+./runAnaAll.sh $scratch'/data/MinimumBias/MB-C10-PR9-TRKANASKIM-v3_proc0607/sub2/trkhists_trkAnaSkimAOD_*.root' $stag
