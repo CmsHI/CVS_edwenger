798f6d5f765bbfd3d01dc4a9a9d9d52ed56a52ac
frankma <>
2010-06-07 15:01:27 +0000

    checked trkAnaSkimAOD_2*_1.root files

--- TrackSpectraAnalyzer/macros/runTop.sh
+++ TrackSpectraAnalyzer/macros/runTop.sh
@@ -2,8 +2,10 @@
 tag=V0531_v8
 
 # run all
-stag=$tag/all
-./runAnaAll.sh $scratch'/data/MinimumBias/MB-C10-PR9-TRKANASKIM-v3_proc0531/trkhists_trkAnaSkimAOD_*.root' $stag
+#stag=$tag/all
+stag=$tag/sub2good
+./runAnaAll.sh $scratch'/data/MinimumBias/MB-C10-PR9-TRKANASKIM-v3_proc0531/sub2good/trkhists_trkAnaSkimAOD_*.root' $stag
+exit
 
 # run sub ranges of files to check results as a function of lumi
 for i in `seq 1 9`; do
