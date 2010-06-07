c4c793ed9d527e1fa299d684a00d5c5ee402946b
frankma <>
2010-06-07 09:49:55 +0000

    do a fresh run with all and sublumis

--- TrackSpectraAnalyzer/macros/runTop.sh
+++ TrackSpectraAnalyzer/macros/runTop.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -
 tag=V0531_v7
-./runAnaAll.sh $scratch'/data/MinimumBias/MB-C10-PR9-TRKANASKIM-v3_proc0531/trkhists_trkAnaSkimAOD_9*.root' $tag
 
-for i in `seq 8 9`; do
+# run all
+./runAnaAll.sh $scratch'/data/MinimumBias/MB-C10-PR9-TRKANASKIM-v3_proc0531/trkhists_trkAnaSkimAOD_*.root' $tag
+
+# run sub ranges of files to check results as a function of lumi
+for i in `seq 1 9`; do
   stag=$tag/sub$i
   ./runAnaAll.sh $scratch'/data/MinimumBias/MB-C10-PR9-TRKANASKIM-v3_proc0531/trkhists_trkAnaSkimAOD_'$i'*.root' $stag
 done
