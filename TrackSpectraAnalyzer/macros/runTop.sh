8038d123c0aef5a54ae77f9ffd1eae9cb490de78
frankma <>
2010-06-08 08:38:55 +0000

    mc reco works\! thank God\!

--- TrackSpectraAnalyzer/macros/runTop.sh
+++ TrackSpectraAnalyzer/macros/runTop.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -
-tag=V0607_v1
+tag=mcproc0607
 
 # run all
-stag=$tag/all
-./runAnaAll.sh $scratch'/data/MinimumBias/MB-C10-PR9-TRKANASKIM-v3_proc0607/trkhists_trkAnaSkimAOD_*.root' $stag
+stag=$tag/sub7
+#./runAnaAll.sh $scratch'/data/MinimumBias/MB-C10-PR9-TRKANASKIM-v3_proc0607/trkhists_trkAnaSkimAOD_*.root' $stag
+./runAnaAll.sh 'dcache:/pnfs/cmsaf.mit.edu/t2bat/cms/store/user/frankma/mc/s10-STARTV26A_356/proc0607/trkhistsMC_*7*_1.root' $stag
+exit
 
 # run sub ranges of files to check results as a function of lumi
 for i in `seq 1 9`; do
