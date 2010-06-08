04dd9e808e8eaa750dbe172b3c54642298ccffff
frankma <>
2010-06-08 09:04:15 +0000

    add sample name and for spectra, trkNtName as parameter input

--- TrackSpectraAnalyzer/macros/runAnaAll.sh
+++ TrackSpectraAnalyzer/macros/runAnaAll.sh
@@ -1,29 +1,30 @@
 #!/bin/bash -
-if [ $# -lt 2 ]; then
+if [ $# -lt 3 ]; then
   echo Usage
-  echo "  $0 <ana_input_file> <tag>"
+  echo "  $0 <sampleName> <ana_input_file> <tag>"
   exit
 fi
 
-effNtAnaInFile=$1
-tag=$2
+sampleName=$1
+effNtAnaInFile=$2
+tag=$3
 
 # === First Get the HLT Turn-on ===
 # use nt
 effNtAnaOutDir=plots/$tag/ntHltEff
-root -b -q anaHltJetEff_simple.C+\(\"$effNtAnaInFile\",\"$effNtAnaOutDir\",false,\"trackAna/\"\)
+root -b -q anaHltJetEff_simple.C+\(\"$sampleName\",\"$effNtAnaInFile\",\"$effNtAnaOutDir\",false,\"trackAna/\"\)
 if [ $? -ne 0 ]; then exit 1; fi
 
 # use hist from nt
 effHisAnaInFile=$effNtAnaOutDir/anahlt.root
 effHisAnaOutDir=plots/$tag/HisHltEff
-root -b -q anaHltJetEff_simple.C+\(\"$effHisAnaInFile\",\"$effHisAnaOutDir\",true,\"\",1\)
+root -b -q anaHltJetEff_simple.C+\(\"$sampleName\",\"$effHisAnaInFile\",\"$effHisAnaOutDir\",true,\"\",1\)
 if [ $? -ne 0 ]; then exit 1; fi
 
 # === Second Plot the Triggered Spectra ===
 trigSpecAnaInFile=$effHisAnaOutDir/anahlt.root
 trigSpecAnaOutDir=plots/$tag/trigSpec
-root -b -q anaTrigSpectra_simple.C+\(\"$trigSpecAnaInFile\",\"$effNtAnaInFile\",\"$trigSpecAnaOutDir\",\"trackAna/\",0.99\)
+root -b -q anaTrigSpectra_simple.C+\(\"$trigSpecAnaInFile\",\"$effNtAnaInFile\",\"$trigSpecAnaOutDir\",\"trackAna/\",\"nt_jettrack\",0.99\)
 if [ $? -ne 0 ]; then exit 1; fi
 
 trigSpecPlotsInFile=$trigSpecAnaOutDir/anaspec.root
