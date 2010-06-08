1a77875975ca04215001198600e43a6314fc13d5
frankma <>
2010-06-08 09:29:59 +0000

    begin to prepare for the gen particle case

--- TrackSpectraAnalyzer/macros/runTop.sh
+++ TrackSpectraAnalyzer/macros/runTop.sh
@@ -1,18 +1,20 @@
 #!/bin/bash -
 tag=mcproc0607
 
+recoTrkNtName="nt_jettrack"
+genTrkNtName="nt_gen_dndptdeta"
 # run all
 stag=$tag/sub375
-#./runAnaAll.sh "PythiaD6T" $scratch'/data/MinimumBias/MB-C10-PR9-TRKANASKIM-v3_proc0607/trkhists_trkAnaSkimAOD_*.root' $stag "nt_jettrack"
-./runAnaAll.sh "Data" 'dcache:/pnfs/cmsaf.mit.edu/t2bat/cms/store/user/frankma/mc/s10-STARTV26A_356/proc0607/trkhistsMC_375_1.root' $stag "nt_jettrack"
+#./runAnaAll.sh "Data" $scratch'/data/MinimumBias/MB-C10-PR9-TRKANASKIM-v3_proc0607/trkhists_trkAnaSkimAOD_*.root' $stag $recoTrkNtName
+./runAnaAll.sh "PythiaD6T" 'dcache:/pnfs/cmsaf.mit.edu/t2bat/cms/store/user/frankma/mc/s10-STARTV26A_356/proc0607/trkhistsMC_375_1.root' $stag $genTrkNtName
 exit
 
 # run sub ranges of files to check results as a function of lumi
 for i in `seq 1 9`; do
   stag=$tag/sub$i
-  ./runAnaAll.sh "PythiaD6T" $scratch'/data/MinimumBias/MB-C10-PR9-TRKANASKIM-v3_proc0607/trkhists_trkAnaSkimAOD_'$i'*.root' $stag "nt_jet_track"
+  ./runAnaAll.sh "Data" $scratch'/data/MinimumBias/MB-C10-PR9-TRKANASKIM-v3_proc0607/trkhists_trkAnaSkimAOD_'$i'*.root' $stag $recoTrkNtName
 done
 
 # 2* treat separately
 stag=$tag/sub2
-./runAnaAll.sh "PythiaD6T" $scratch'/data/MinimumBias/MB-C10-PR9-TRKANASKIM-v3_proc0607/sub2/trkhists_trkAnaSkimAOD_*.root' $stag "nt_jet_track"
+./runAnaAll.sh "Data" $scratch'/data/MinimumBias/MB-C10-PR9-TRKANASKIM-v3_proc0607/sub2/trkhists_trkAnaSkimAOD_*.root' $stag $recoTrkNtName
