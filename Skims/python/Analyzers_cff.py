f000692a9347fc3a1a969f5cf2fddbd6231a4ec4
sungho <>
2010-06-09 13:35:48 +0000

    evt sel eff to higher multiplicity

--- Skims/python/Analyzers_cff.py
+++ Skims/python/Analyzers_cff.py
@@ -37,16 +37,18 @@ looseTrackAna_STD = trackAna.clone(src=cms.untracked.InputTag("looseSelectTracks
                                    applyEvtEffCorr=cms.untracked.bool(True),
                                    evtEffCorrType=cms.untracked.int32(0),
                                    evtMultCut=cms.untracked.int32(0),
-                                   evtSelEffv = cms.untracked.vdouble(20,0.0266389,0.352064,0.55773,0.709436,0.821248,0.89875,0.956152,0.995745,1.01817,1.02657,
-                                                                      1.02549,1.01831,1.01144,1.00505,0.998037,0.994116,0.992235,0.991112,0.991103,0.990337))
+                                   #evtMultCut=cms.untracked.int32(3),
+                                   evtSelEffv = cms.untracked.vdouble(30,0.0266389,0.352064,0.55773,0.709436,0.821248,0.89875,0.956152,0.995745,1.01817,1.02657,1.02549,1.01831,
+                                                                      1.01144,1.00505,0.998037,0.994116,0.992235,0.991112,0.991103,0.990337,0.991335,0.991312,0.991832,
+                                                                      0.992135,0.992577,0.992819,0.993234,0.993617,0.993562,0.993773))
 
 trackAna_STD = trackAna.clone(src = cms.untracked.InputTag("selectTracks"),
                               applyEvtEffCorr=cms.untracked.bool(True),
                               evtEffCorrType=cms.untracked.int32(0),
                               evtMultCut=cms.untracked.int32(3),
-                              evtSelEffv = cms.untracked.vdouble(20,0,9.0384e-05,0.00223004,0.0304791,0.217088,0.460085,0.673905,0.830166,0.92802,0.978678,
-                                                                 1.00044,1.00497,1.00382,1.00041,0.994874,0.99174,0.990136,0.989347,0.989595,0.989071))
-
+                              evtSelEffv = cms.untracked.vdouble(30,0,9.0384e-05,0.00223004,0.0304791,0.217088,0.460085,0.673905,0.830166,0.92802,0.978678,1.00044,
+                                                                 1.00497,1.00382,1.00041,0.994874,0.99174,0.990136,0.989347,0.989595,0.989071,0.990078,0.990065,0.990801,
+                                                                 0.991051,0.991619,0.99187,0.992327,0.992703,0.99281,0.992914))
 
 # tracking efficiency analyzer
 from edwenger.TrkEffAnalyzer.trkEffAnalyzer_cff import *
