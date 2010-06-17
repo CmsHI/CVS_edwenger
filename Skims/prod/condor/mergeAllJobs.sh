7bbc6e9fdb40aceca70219474dc24b8d77532182
frankma <>
2010-06-17 13:50:52 +0000

    merge proc0615_nt, iteration 0

--- Skims/prod/condor/mergeAllJobs.sh
+++ Skims/prod/condor/mergeAllJobs.sh
@@ -1,22 +1,25 @@
 topDir=/net/hibat0003/d00/scratch/frankma/data/MinimumBias
 jobs="\
-MB-C10-A20RR-TRKANASKIM-v2_proc0615_TrkCutDefault \
-MB-C10-A20RR-TRKANASKIM-v2_proc0615_TrkCutV1 \
-MB-C10-A20RR-TRKANASKIM-v2_proc0615_TrkCutV2 \
-MB-C10-A20RR-TRKANASKIM-v2_proc0615_TrkCutV3 \
-MB-C10-A20RR-TRKANASKIM-v3_proc0615_TrkCutDefault \
-MB-C10-A20RR-TRKANASKIM-v3_proc0615_TrkCutV1 \
-MB-C10-A20RR-TRKANASKIM-v3_proc0615_TrkCutV2 \
-MB-C10-A20RR-TRKANASKIM-v3_proc0615_TrkCutV3 \
-MB-C10-PR9-TRKANASKIM-v3_proc0615_TrkCutDefault \
-MB-C10-PR9-TRKANASKIM-v3_proc0615_TrkCutV1 \
-MB-C10-PR9-TRKANASKIM-v3_proc0615_TrkCutV2 \
-MB-C10-PR9-TRKANASKIM-v3_proc0615_TrkCutV3 \
+$topDir/MB-C10-A20RR-TRKANASKIM-v2_proc0615_nt \
+$topDir/MB-C10-A20RR-TRKANASKIM-v3_proc0615_nt \
+$topDir/MB-C10-M6RR-JMTskim-v0_proc0615_nt \
+$topDir/MB-C10-M6RR-MBskim-v0_proc0615_nt \
+$topDir/MB-C10-PR9-JMTskim-v0_proc0615_nt \
+$topDir/MB-C10-PR9-MBskim-v0_proc0615_nt \
+$topDir/MB-C10-PR9-TRKANASKIM-v3_proc0615_nt \
 "
 
+topDir=/net/hibat0003/d00/scratch/frankma/data/JetMETTau
+jobs=$jobs"\
+$topDir/JMT-R10A-PR1-JMTskim-v0_proc0615_nt \
+"
+
+# merge level
+level=
+#level="/merge"
 for i in $jobs; do
-  echo $i
-  cmd="./mergeJob.sh $topDir/$i/merge trkhists"
+  #echo $i
+  cmd="./mergeJob.sh ${i}${level} trkhists"
   echo $cmd
   eval $cmd
 done
