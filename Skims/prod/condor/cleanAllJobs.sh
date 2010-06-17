393965e968142fd1c942a2f4c6c64b4975c3e944
frankma <>
2010-06-17 13:28:33 +0000

    clean up proc0615_nt

--- Skims/prod/condor/cleanAllJobs.sh
+++ Skims/prod/condor/cleanAllJobs.sh
@@ -1,23 +1,25 @@
-scratch=/net/hibat0003/d00/scratch/frankma/data/MinimumBias
+#!/bin/bash -
 startdir=$PWD
+
+scratch=/net/hibat0003/d00/scratch/frankma/data/MinimumBias
 jobs="\
-MB-C10-A20RR-TRKANASKIM-v2_proc0615_TrkCutDefault \
-MB-C10-A20RR-TRKANASKIM-v2_proc0615_TrkCutV1 \
-MB-C10-A20RR-TRKANASKIM-v2_proc0615_TrkCutV2 \
-MB-C10-A20RR-TRKANASKIM-v2_proc0615_TrkCutV3 \
-MB-C10-A20RR-TRKANASKIM-v3_proc0615_TrkCutDefault \
-MB-C10-A20RR-TRKANASKIM-v3_proc0615_TrkCutV1 \
-MB-C10-A20RR-TRKANASKIM-v3_proc0615_TrkCutV2 \
-MB-C10-A20RR-TRKANASKIM-v3_proc0615_TrkCutV3 \
-MB-C10-PR9-TRKANASKIM-v3_proc0615_TrkCutDefault \
-MB-C10-PR9-TRKANASKIM-v3_proc0615_TrkCutV1 \
-MB-C10-PR9-TRKANASKIM-v3_proc0615_TrkCutV2 \
-MB-C10-PR9-TRKANASKIM-v3_proc0615_TrkCutV3 \
+$scratch/MB-C10-A20RR-TRKANASKIM-v2_proc0615_nt \
+$scratch/MB-C10-A20RR-TRKANASKIM-v3_proc0615_nt \
+$scratch/MB-C10-M6RR-JMTskim-v0_proc0615_nt \
+$scratch/MB-C10-M6RR-MBskim-v0_proc0615_nt \
+$scratch/MB-C10-PR9-JMTskim-v0_proc0615_nt \
+$scratch/MB-C10-PR9-MBskim-v0_proc0615_nt \
+$scratch/MB-C10-PR9-TRKANASKIM-v3_proc0615_nt \
+"
+
+scratch=/net/hibat0003/d00/scratch/frankma/data/JetMETTau
+jobs=$jobs"\
+$scratch/JMT-R10A-PR1-JMTskim-v0_proc0615_nt \
 "
 
 for i in $jobs; do
   echo $i
-  cd $scratch/$i
+  cd $i
   echo "  # of files:" `ls trkhists*.root | wc -l`
   cmd=../scripts/cleanJob.sh
   echo "  run: $cmd"
