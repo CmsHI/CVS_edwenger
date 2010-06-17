6d2b5767d1c9d9fc9f45040a7df661882cccab5a
frankma <>
2010-06-17 12:41:11 +0000

    fix mistaken: the last subgroup was not merged

--- Skims/prod/condor/mergeJob.sh
+++ Skims/prod/condor/mergeJob.sh
@@ -1,31 +1,36 @@
 #~/bin/bash -
 if [ $# -lt 2 ]; then
   echo Usage:
-  echo "  $0 <topDir> <job>"
+  echo "  $0 <inDir> <tag>"
+  echo "  * inDir is a dir containing the root files you want to merge"
   exit 1
 fi
 
 script=mergeFiles.sh
-topDir=$1
-job=$2
+inDir=$1
+tag=$2
 
-outdir=$topDir/$job/merge
+# initialize
+outdir=$inDir/merge
 ct=0
 cmd=
-N=100000000
 nPerMerge=20
+N=`ls $inDir/*.root | grep $tag | wc -l`
 
-for job in `ls $topDir/$job/trkhists*.root | head -n $N`; do
+for job in `ls $inDir/*.root | grep $tag | head -n $N`; do
   subi=`expr $ct % $nPerMerge`
+  echo $job, subi: $subi
+  # -- build subgroup --
   if [ $subi -eq 0 ]; then
-    if [ -n "$cmd" ]; then
-      #echo $cmd
-      eval $cmd
-    fi
     cmd="./condor64Basic.sh $script $outdir trkhists_sub$ct.root"
   else
     #echo $ct: $job
     cmd="$cmd $job"
+  fi
+  # -- merge subgroup --
+  if [ $subi -eq $((nPerMerge-1)) -o $ct -eq $((N-1)) ]; then
+    echo $cmd
+    #eval $cmd
   fi
   ct=$((ct+1))
 done
