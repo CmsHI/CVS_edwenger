0593c0a9dad265b11626ee58be0a89729e1c2d6f
frankma <>
2010-06-25 15:52:28 +0000

    proc0625: clean up

--- Skims/prod/condor/cleanAllJobs.sh
+++ Skims/prod/condor/cleanAllJobs.sh
@@ -1,20 +1,21 @@
 #!/bin/bash -
 startdir=$PWD
 
+jobTag=proc0625
 topDir=/net/hibat0003/d00/scratch/frankma/data/MinimumBias
 jobs="\
-$topDir/MB-C10-A20RR-TRKANASKIM-v2_proc0615_nt \
-$topDir/MB-C10-A20RR-TRKANASKIM-v3_proc0615_nt \
-$topDir/MB-C10-M6RR-JMTskim-v0_proc0615_nt \
-$topDir/MB-C10-M6RR-MBskim-v0_proc0615_nt \
-$topDir/MB-C10-PR9-JMTskim-v0_proc0615_nt \
-$topDir/MB-C10-PR9-MBskim-v0_proc0615_nt \
-$topDir/MB-C10-PR9-TRKANASKIM-v3_proc0615_nt \
+$topDir/MB-C10-A20RR-TRKANASKIM-v2_$jobTag \
+$topDir/MB-C10-A20RR-TRKANASKIM-v3_$jobTag \
+$topDir/MB-C10-M6RR-JMTskim-v0_$jobTag \
+$topDir/MB-C10-M6RR-MBskim-v0_$jobTag \
+$topDir/MB-C10-PR9-JMTskim-v0_$jobTag \
+$topDir/MB-C10-PR9-MBskim-v0_$jobTag \
+$topDir/MB-C10-PR9-TRKANASKIM-v3_$jobTag \
 "
 
 topDir=/net/hibat0003/d00/scratch/frankma/data/JetMETTau
 jobs=$jobs"\
-$topDir/JMT-R10A-PR1-JMTskim-v0_proc0615_nt \
+$topDir/JMT-R10A-PR1-JMTskim-v0_$jobTag \
 "
 
 for i in $jobs; do
