1e9324a84147b571c217a179a5b55ba5ffce3ee6
frankma <>
2010-06-25 15:49:11 +0000

    proc0625: merge iteration 0

--- Skims/prod/condor/mergeAllJobs.sh
+++ Skims/prod/condor/mergeAllJobs.sh
@@ -1,22 +1,23 @@
 topDir=/net/hibat0003/d00/scratch/frankma/data/MinimumBias
+jobTag=proc0625
 jobs="\
-$topDir/MB-C10-A20RR-TRKANASKIM-v2_proc0615_nt \
-$topDir/MB-C10-A20RR-TRKANASKIM-v3_proc0615_nt \
-$topDir/MB-C10-M6RR-JMTskim-v0_proc0615_nt \
-$topDir/MB-C10-M6RR-MBskim-v0_proc0615_nt \
-$topDir/MB-C10-PR9-JMTskim-v0_proc0615_nt \
-$topDir/MB-C10-PR9-MBskim-v0_proc0615_nt \
-$topDir/MB-C10-PR9-TRKANASKIM-v3_proc0615_nt \
+$topDir/MB-C10-A20RR-TRKANASKIM-v2_$jobTag \
+$topDir/MB-C10-A20RR-TRKANASKIM-v3_$jobTag \
+$topDir/MB-C10-M6RR-JMTskim-v0_$jobTag \
+$topDir/MB-C10-M6RR-MBskim-v0_$jobTag \
+$topDir/MB-C10-PR9-JMTskim-v0_$jobTag \
+$topDir/MB-C10-PR9-MBskim-v0_$jobTag \
+$topDir/MB-C10-PR9-TRKANASKIM-v3_$jobTag \
 "
 
 topDir=/net/hibat0003/d00/scratch/frankma/data/JetMETTau
 jobs=$jobs"\
-$topDir/JMT-R10A-PR1-JMTskim-v0_proc0615_nt \
+$topDir/JMT-R10A-PR1-JMTskim-v0_$jobTag \
 "
 
 # merge level
-#level=
-level="/merge"
+level=
+#level="/merge"
 for i in $jobs; do
   #echo $i
   cmd="./mergeJob.sh ${i}${level} trkhists"
