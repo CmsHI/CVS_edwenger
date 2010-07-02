63ee0d5c45181d23957bc2ba39eac9d63d6c0003
sungho <>
2010-07-02 22:41:11 +0000

    variable binning

--- TrkEffAnalyzer/src/TrkEffHistograms.cc
+++ TrkEffAnalyzer/src/TrkEffHistograms.cc
@@ -46,16 +46,16 @@ TrkEffHistograms::declareHistograms()
     if(!constPtBins){
        const double small = 1e-3;
        double pt;
-       
+
        for(pt =   0; pt <   1-small; pt +=  0.05) ptBins.push_back(pt);
        for(pt =   1; pt <   2-small; pt +=  0.1 ) ptBins.push_back(pt);
-       for(pt =   2; pt <   5-small; pt +=  0.2 ) ptBins.push_back(pt);
-       for(pt =   5; pt <  10-small; pt +=  0.5 ) ptBins.push_back(pt);
+       for(pt =   2; pt <   6-small; pt +=  0.2 ) ptBins.push_back(pt);
+       for(pt =   6; pt <  10-small; pt +=  0.5 ) ptBins.push_back(pt);
        for(pt =  10; pt <  20-small; pt +=  1.0 ) ptBins.push_back(pt);
-       for(pt =  20; pt <  50-small; pt +=  2.0 ) ptBins.push_back(pt);
+       for(pt =  20; pt <  50-small; pt +=  3.0 ) ptBins.push_back(pt);
        for(pt =  50; pt < 100-small; pt +=  5.0 ) ptBins.push_back(pt);
        for(pt = 100; pt < 200-small; pt += 10.0 ) ptBins.push_back(pt);
-       for(pt = 200; pt < 500-small; pt += 20.0 ) ptBins.push_back(pt);
+       for(pt = 200; pt < 400-small; pt += 20.0 ) ptBins.push_back(pt);
 
     }else if(lowPtMode){
 
