ee097a613cba11945da286b4edf83db46f948cea
edwenger <>
2010-07-05 08:06:00 +0000

    cff to use custom JEX beamspot

---
+++ JulyExercise/python/BeamSpotJEX_cff.py
@@ -0,0 +1,13 @@
+import FWCore.ParameterSet.Config as cms
+from CondCore.DBCommon.CondDBSetup_cfi import        *
+from RecoVertex.BeamSpotProducer.BeamSpot_cfi import *
+
+BeamSpotDBSource = cms.ESSource("PoolDBESSource",
+                                CondDBSetup,
+                                toGet = cms.VPSet(cms.PSet(
+    record = cms.string('BeamSpotObjectsRcd'),
+    tag = cms.string('JEX_BeamSpot_Preliminary'))),
+                                connect = cms.string('sqlite_file:../test/JEX_BeamSpot_Preliminary.db')
+                                )
+es_prefer_beamspot = cms.ESPrefer("PoolDBESSource","BeamSpotDBSource")
+
