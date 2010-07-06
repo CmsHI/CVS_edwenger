ef7be61658cb76359aab726948e9f224b09d7165
edwenger <>
2010-07-06 09:42:18 +0000

    move centrality tables to data directory

--- JulyExercise/test/testCentralityBinning_cfg.py
+++ JulyExercise/test/testCentralityBinning_cfg.py
@@ -8,13 +8,14 @@ process = cms.Process("TEST")
 process.load("CondCore.DBCommon.CondDBCommon_cfi")
 
 process.CondDBCommon.connect = 'sqlite_file:CentralityTables.db'
+#process.CondDBCommon.connect = 'sqlite_file:CentralityTablesMC.db'
 
 process.PoolDBESSource = cms.ESSource("PoolDBESSource",
                                        process.CondDBCommon,
                                        DumpStat=cms.untracked.bool(True),
                                        toGet = cms.VPSet(cms.PSet(
                                                record = cms.string('HeavyIonRcd'),
-                                               tag = cms.string('HFhits40_MC_Hydjet2760GeV_MC_3XY_V24_v0')
+                                               tag = cms.string('HFhits40_DataJulyExercise_Hydjet2760GeV_MC_37Y_V5_v0')
                                                 )),
                                       )
 
@@ -24,8 +25,9 @@ process.maxEvents = cms.untracked.PSet(
 
 process.source = cms.Source("PoolSource",
                             fileNames = cms.untracked.vstring(
-    "file:/net/hisrv0001/home/edwenger/jex/edwenger/JulyExercise/python/hiRecoJEX_RAW2DIGI_RECO.root"
-    )
+    #"file:/net/hisrv0001/home/edwenger/scratch/jex/599a3a4efd1a164b86e329cbf44e9f6a/hiRecoJEX_RAW2DIGI_RECO_29_1_2Ko.root"
+    "dcache:/pnfs/cmsaf.mit.edu/t2bat/cms/store/user/kimy/MinimumBiasHI/Spring10-JulyAnalysisExercise_MC_37Y_V4-RECO-prelim-v1/599a3a4efd1a163b86e329cbf44e9f6a/hiRecoJEX_RAW2DIGI_RECO_29_1_2Ko.root"
+	)
                             )
 
 process.analyze = cms.EDAnalyzer("AnalyzerWithCentrality")
