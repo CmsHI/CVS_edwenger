4c19b851b6021cd955b1ac04a4ecfbd3d7f7b774
frankma <>
2010-07-10 18:09:11 +0000

    fix typo

--- Skims/prod/condor/submitAll.sh
+++ Skims/prod/condor/submitAll.sh
@@ -7,25 +7,25 @@ jobTag=proc0710_trkAnaNoFilter
 # ===================== 358p3 =====================
 job=MB-C10-M6RR-MBskim-v0
 inputList=pub_list/$job.txt
-output_dir=$scrath/data/MinimumBias/${job}_${jobTag}
+output_dir=$scratch/data/MinimumBias/${job}_${jobTag}
 ./submitJob.sh $inputList $output_dir
 
 job=MB-C10-M6RR-JMTskim-v0
 inputList=pub_list/$job.txt
-output_dir=$scrath/data/MinimumBias/${job}_${jobTag}
+output_dir=$scratch/data/MinimumBias/${job}_${jobTag}
 ./submitJob.sh $inputList $output_dir
 
 job=MB-C10-PR9-JMTskim-v0
 inputList=pub_list/$job.txt
-output_dir=$scrath/data/MinimumBias/${job}_${jobTag}
+output_dir=$scratch/data/MinimumBias/${job}_${jobTag}
 ./submitJob.sh $inputList $output_dir
 
 job=MB-C10-PR9-MBskim-v0
 inputList=pub_list/$job.txt
-output_dir=$scrath/data/MinimumBias/${job}_${jobTag}
+output_dir=$scratch/data/MinimumBias/${job}_${jobTag}
 ./submitJob.sh $inputList $output_dir
 
 job=JMT-R10A-PR1-JMTskim-v0
 inputList=pub_list/$job.txt
-output_dir=$scrath/data/JetMETTau/${job}_${jobTag}
+output_dir=$scratch/data/JetMETTau/${job}_${jobTag}
 ./submitJob.sh $inputList $output_dir
