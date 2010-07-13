400476cbd2d3cf8cf03499d0b4559ea731c2ee2a
frankma <>
2010-07-13 16:21:29 +0000

    merging for proc0710

--- Skims/prod/condor/mergeAllJobs.sh
+++ Skims/prod/condor/mergeAllJobs.sh
@@ -1,24 +1,21 @@
-jobTag=proc0628_trkAnaNoFilter_v2
-jobTagFilter=proc0628_trkAnaFilter_v2
-topDir=/net/hibat0003/d00/scratch/frankma/data/MinimumBias
+jobTag=proc0710_trkAnaNoFilter
+scratch=/net/hibat0007/d00/scratch/frankma
+topDir=$scratch/data/MinimumBias
 jobs="\
-$topDir/MB-C10-A20RR-TRKANASKIM-v2_$jobTag \
-$topDir/MB-C10-A20RR-TRKANASKIM-v3_$jobTag \
 $topDir/MB-C10-M6RR-JMTskim-v0_$jobTag \
 $topDir/MB-C10-M6RR-MBskim-v0_$jobTag \
 $topDir/MB-C10-PR9-JMTskim-v0_$jobTag \
 $topDir/MB-C10-PR9-MBskim-v0_$jobTag \
-$topDir/MB-C10-PR9-TRKANASKIM-v3_$jobTagFilter \
 "
 
-topDir=/net/hibat0003/d00/scratch/frankma/data/JetMETTau
+topDir=$scratch/data/JetMETTau
 jobs=$jobs"\
 $topDir/JMT-R10A-PR1-JMTskim-v0_$jobTag \
 "
 
 # merge inFolder
 inFolder="."
-outFolder="mergeAll_v2"
+outFolder="mergeAll"
 for i in $jobs; do
   #echo $i
   cmd="./mergeJob.sh ${i}/${inFolder} trkhists ${i}/${outFolder}"
