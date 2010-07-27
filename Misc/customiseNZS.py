b1a68724fa61a7eac1c82d608deca4abd0aa207f
edwenger <>
2010-07-27 14:37:42 +0000

    fix indentation

--- Misc/customiseNZS.py
+++ Misc/customiseNZS.py
@@ -15,41 +15,41 @@ def customiseStripCommon(process):
     process.simSiStripDigis.ZeroSuppression = cms.bool(False)
     process.SiStripDigiToRaw.InputDigiLabel = cms.string('VirginRaw')
     process.SiStripDigiToRaw.FedReadoutMode = cms.string('VIRGIN_RAW')
-
+    
     process.simSiStripDigis.Noise = cms.bool(True)
     process.simSiStripDigis.TrackerConfigurationFromDB = cms.bool(True)
-
+    
     process.poolDBESSource = cms.ESSource("PoolDBESSource",
-       BlobStreamerName = cms.untracked.string('TBufferBlobStreamingService'),
-       DBParameters = cms.PSet(
-           messageLevel = cms.untracked.int32(2),
-           authenticationPath = cms.untracked.string('/afs/cern.ch/cms/DB/conddb')
-       ),
-       timetype = cms.untracked.string('runnumber'),
-       connect = cms.string('frontier://FrontierProd/CMS_COND_31X_STRIP'),
-       toGet = cms.VPSet(
-           cms.PSet(
-               record = cms.string('SiStripNoisesRcd'),
-               tag = cms.string('SiStripNoise_CRAFT09_DecMode_ForTrackerSim')
-           ),
-           cms.PSet(
-               record = cms.string('SiStripPedestalsRcd'),
-               tag = cms.string('SiStripPedestal_CRAFT09_DecMode_ForTrackerSim')
-           ),
-           cms.PSet(
-               record = cms.string('SiStripFedCablingRcd'),
-               tag = cms.string('SiStripFedCabling_CRAFT09_ForTrackerSim')
-           ),
-           cms.PSet(
-               record = cms.string('SiStripBadChannelRcd'),
-               tag = cms.string('SiStripBadChannelsFromO2O_CRAFT09_DecMode_ForTrackerSim')
-           )
-       )
-   )
-
-   process.es_prefer_my =cms.ESPrefer("PoolDBESSource","poolDBESSource")
-
-
+                                          BlobStreamerName = cms.untracked.string('TBufferBlobStreamingService'),
+                                          DBParameters = cms.PSet(
+        messageLevel = cms.untracked.int32(2),
+        authenticationPath = cms.untracked.string('/afs/cern.ch/cms/DB/conddb')
+        ),
+                                          timetype = cms.untracked.string('runnumber'),
+                                          connect = cms.string('frontier://FrontierProd/CMS_COND_31X_STRIP'),
+                                          toGet = cms.VPSet(
+        cms.PSet(
+        record = cms.string('SiStripNoisesRcd'),
+        tag = cms.string('SiStripNoise_CRAFT09_DecMode_ForTrackerSim')
+        ),
+        cms.PSet(
+        record = cms.string('SiStripPedestalsRcd'),
+        tag = cms.string('SiStripPedestal_CRAFT09_DecMode_ForTrackerSim')
+        ),
+        cms.PSet(
+        record = cms.string('SiStripFedCablingRcd'),
+        tag = cms.string('SiStripFedCabling_CRAFT09_ForTrackerSim')
+        ),
+        cms.PSet(
+        record = cms.string('SiStripBadChannelRcd'),
+        tag = cms.string('SiStripBadChannelsFromO2O_CRAFT09_DecMode_ForTrackerSim')
+        )
+        )
+                                          )
+    
+    process.es_prefer_my =cms.ESPrefer("PoolDBESSource","poolDBESSource")
+    
+    
 def customiseStripCMN(process):
     process = customiseStripCommon(process)
     process.simSiStripDigis.CommonModeNoise = cms.bool(True)
