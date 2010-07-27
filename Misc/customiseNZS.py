f24c416b87605da30f65a6647c5e2929a3b66e0d
edwenger <>
2010-07-27 15:19:48 +0000

    drop simSiStripDigis and FEDRawDataCollection from first DIGI,DIGI2RAW

--- Misc/customiseNZS.py
+++ Misc/customiseNZS.py
@@ -56,17 +56,28 @@ def customiseStripDB(process):
 def customiseStripCommon(process):
 
     process = customiseStripDB(process)
-    
+
+    # digitize in Virgin Raw
     process.simSiStripDigis.ZeroSuppression = cms.bool(False)
     process.SiStripDigiToRaw.InputDigiLabel = cms.string('VirginRaw')
     process.SiStripDigiToRaw.FedReadoutMode = cms.string('VIRGIN_RAW')
-    
-    process.simSiStripDigis.Noise = cms.bool(True)
     process.simSiStripDigis.TrackerConfigurationFromDB = cms.bool(True)
 
+    # turn silicon effects off unless otherwise specified later
+    process.simSiStripDigis.BaselineShift = cms.bool(False)
+    process.simSiStripDigis.APVSaturationFromHIP = cms.bool(False)
+    process.simSiStripDigis.CommonModeNoise = cms.bool(False)
+
+    # only do redigi of strip tracker
     process.pdigi.remove(process.addPileupInfo)
     process.pdigi.remove(process.trackingParticles)
     process.pdigi.replace(process.doAllDigi,process.simSiStripDigis)
+
+    # drop existing tracker digis and raw data
+    process.source.inputCommands = cms.untracked.vstring('keep *',
+                                                         'drop FEDRawDataCollection_*_*_*',
+                                                         'drop *_simSiStripDigis_*_*')
+    process.source.dropDescendantsOfDroppedBranches=cms.untracked.bool(False)
 
     return process
 
