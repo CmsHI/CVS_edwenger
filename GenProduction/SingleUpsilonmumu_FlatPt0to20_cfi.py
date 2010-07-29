e3ff8e0697b89808f73e3785012a33a1b1107b41
edwenger <>
2010-07-29 12:56:34 +0000

    tidy up pythia gun configs

--- GenProduction/SingleUpsilonmumu_FlatPt0to20_cfi.py
+++ GenProduction/SingleUpsilonmumu_FlatPt0to20_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-from Configuration.Generator.PythiaUESettings_cfi import *
+from Configuration.Generator.PyquenDefaultSettings_cff import *
 hiSignal = cms.EDProducer("Pythia6PtYDistGun",
     PGunParameters = cms.PSet(
         ParticleID = cms.vint32(553),
@@ -9,53 +9,22 @@ hiSignal = cms.EDProducer("Pythia6PtYDistGun",
         YBinning = cms.int32(500),
         MaxPt = cms.double(20.0),
         MinPt = cms.double(0.0),
-        MaxY = cms.double(5.0),
-        MinY = cms.double(-5.0),
+        MaxY = cms.double(2.5),
+        MinY = cms.double(-2.5),
         MinPhi = cms.double(-3.1415926535897932384),
         MaxPhi = cms.double(3.1415926535897932384)
     ),
     PythiaParameters = cms.PSet(
-        pythiaUESettingsBlock,
-        UpsilonToMuons = cms.vstring('BRAT(1034) = 0 ',  # switch off',
-                                     'BRAT(1035) = 1 ',  # switch on',
-                                     'BRAT(1036) = 0 ',  # switch off',
-                                     'BRAT(1037) = 0 ',  # switch off',
-                                     'BRAT(1038) = 0 ',  # switch off',
-                                     'BRAT(1039) = 0 ',  # switch off',
-                                     'BRAT(1040) = 0 ',  # switch off',
-                                     'BRAT(1041) = 0 ',  # switch off',
-                                     'BRAT(1042) = 0 ',  # switch off',
-                                     'MDME(1034,1) = 0 ',  # switch off',
-                                     'MDME(1035,1) = 1 ',  # switch on',
-                                     'MDME(1036,1) = 0 ',  # switch off',
-                                     'MDME(1037,1) = 0 ',  # switch off',
-                                     'MDME(1038,1) = 0 ',  # switch off',
-                                     'MDME(1039,1) = 0 ',  # switch off',
-                                     'MDME(1040,1) = 0 ',  # switch off',
-                                     'MDME(1041,1) = 0 ',  # switch off',
-                                     'MDME(1042,1) = 0 ',  # switch off'
-                                     ),
+        pyquenPythiaDefaultBlock,
         parameterSets = cms.vstring(
             'pythiaUESettings',
-            'UpsilonToMuons'
+            'pythiaUpsilonToMuons'
         )
     )
 )
 
-mumugenfilter = cms.EDFilter("MCParticlePairFilter",
-    Status = cms.untracked.vint32(1, 1),
-    MinPt = cms.untracked.vdouble(2.5, 2.5),
-    MaxEta = cms.untracked.vdouble(2.5, 2.5),
-    MinEta = cms.untracked.vdouble(-2.5, -2.5),
-    ParticleCharge = cms.untracked.int32(-1),
-    ParticleID1 = cms.untracked.vint32(13),
-    ParticleID2 = cms.untracked.vint32(13)
-)
-
 configurationMetadata = cms.untracked.PSet(
     version = cms.untracked.string('$Revision: 1.1 $'),
-    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/UserCode/edwenger/GenProduction/Pyquen_SingleUpsilonmumu_FlatPt0to20_cfi.py,v $'),
+    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/UserCode/edwenger/GenProduction/SingleUpsilonmumu_FlatPt0to20_cfi.py,v $'),
     annotation = cms.untracked.string('Pythia Gun Y -> mu mu')
     )
-
-ProductionFilterSequence = cms.Sequence(hiSignal*mumugenfilter)
--- GenProduction/SingleZmumu_FlatPt0to20_cfi.py
+++ GenProduction/SingleZmumu_FlatPt0to20_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-from Configuration.Generator.PythiaUESettings_cfi import *
+from Configuration.Generator.PyquenDefaultSettings_cff import *
 hiSignal = cms.EDProducer("Pythia6PtYDistGun",
     PGunParameters = cms.PSet(
         ParticleID = cms.vint32(23),
@@ -9,47 +9,22 @@ hiSignal = cms.EDProducer("Pythia6PtYDistGun",
         YBinning = cms.int32(500),
         MaxPt = cms.double(20.0),
         MinPt = cms.double(0.0),
-        MaxY = cms.double(5.0),
-        MinY = cms.double(-5.0),
+        MaxY = cms.double(2.5),
+        MinY = cms.double(-2.5),
         MinPhi = cms.double(-3.1415926535897932384),
         MaxPhi = cms.double(3.1415926535897932384)
     ),
     PythiaParameters = cms.PSet(
-        pythiaUESettingsBlock,
-        ZToMuons = cms.vstring("MDME(174,1)=0",          # !Z decay into d dbar,
-                               "MDME(175,1)=0",          # !Z decay into u ubar,
-                               "MDME(176,1)=0",          # !Z decay into s sbar,
-                               "MDME(177,1)=0",          # !Z decay into c cbar,
-                               "MDME(178,1)=0",          # !Z decay into b bbar,
-                               "MDME(179,1)=0",          # !Z decay into t tbar,
-                               "MDME(182,1)=0",          # !Z decay into e- e+,
-                               "MDME(183,1)=0",          # !Z decay into nu_e nu_ebar,
-                               "MDME(184,1)=1",          # !Z decay into mu- mu+,
-                               "MDME(185,1)=0",          # !Z decay into nu_mu nu_mubar,
-                               "MDME(186,1)=0",          # !Z decay into tau- tau+,
-                               "MDME(187,1)=0"           # !Z decay into nu_tau nu_taubar
-                               ),
+        pyquenPythiaDefaultBlock,
         parameterSets = cms.vstring(
             'pythiaUESettings',
-            'ZToMuons'
+            'pythiaZtoMuons'
         )
     )
 )
 
-mumugenfilter = cms.EDFilter("MCParticlePairFilter",
-    Status = cms.untracked.vint32(1, 1),
-    MinPt = cms.untracked.vdouble(2.5, 2.5),
-    MaxEta = cms.untracked.vdouble(2.5, 2.5),
-    MinEta = cms.untracked.vdouble(-2.5, -2.5),
-    ParticleCharge = cms.untracked.int32(-1),
-    ParticleID1 = cms.untracked.vint32(13),
-    ParticleID2 = cms.untracked.vint32(13)
-)
-
 configurationMetadata = cms.untracked.PSet(
     version = cms.untracked.string('$Revision: 1.1 $'),
-    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/UserCode/edwenger/GenProduction/Pyquen_SingleZmumu_FlatPt0to20_cfi.py,v $'),
+    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/UserCode/edwenger/GenProduction/SingleZmumu_FlatPt0to20_cfi.py,v $'),
     annotation = cms.untracked.string('Pythia Gun Z -> mu mu')
     )
-
-ProductionFilterSequence = cms.Sequence(hiSignal*mumugenfilter)
