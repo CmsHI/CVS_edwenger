d9f3cf9a9b41a683d7156021f835d67274a81a6b
edwenger <>
2010-08-05 11:03:38 +0000

    add IteratedMedian.

--- Misc/customiseCMN.py
+++ Misc/customiseCMN.py
@@ -1,6 +1,18 @@
 import FWCore.ParameterSet.Config as cms
 
 ##############################################################################
+def customiseIteratedMedian(process):
+
+    process = customiseStripDB(process)
+    
+    process.siStripZeroSuppression.Algorithms.CommonModeNoiseSubtractionMode=cms.string("IteratedMedian")
+    process.siStripZeroSuppression.Algorithms.CutToAvoidSignal = cms.double(2.0)
+    process.siStripZeroSuppression.Algorithms.Iterations = cms.int32(3)
+    process.siStripZeroSuppression.storeCM = cms.bool(True)
+
+    return process
+
+##############################################################################
 def customisePercentile(process):
 
     process = customiseStripDB(process)
