a5fd3c8ea70db80506be1bcf07e8f06433486c58
appeltel <>
2010-08-16 08:56:49 +0000

    fixed directory problems

--- Misc/setupCMN.sh
+++ Misc/setupCMN.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 dir=RecoLocalTracker/SiStripZeroSuppression
+dir2=`pwd`
 
+cd $CMSSW_BASE/src
 cvs co -r HIStudies_branch_v0 $dir
-cp *CMNSubtractor.h $CMSSW_BASE/$dir/interface
-cp *CMNSubtractor.cc SiStripRawProcessingFactory.cc $CMSSW_BASE/$dir/src
-cp customiseCMN.py DefaultAlgorithms_cff.py $CMSSW_BASE/$dir/python
+cd $dir2
+cp *CMNSubtractor.h $CMSSW_BASE/src/$dir/interface
+cp *CMNSubtractor.cc SiStripRawProcessingFactory.cc $CMSSW_BASE/src/$dir/src
+cp customiseCMN.py DefaultAlgorithms_cff.py $CMSSW_BASE/src/$dir/python
