8f33ae2af8d52960a65881154886c3ab0ba734c8
sungho <>
2010-08-30 11:05:15 +0000

    eventFilterLoose added

--- Skims/python/EventFilter_cff.py
+++ Skims/python/EventFilter_cff.py
@@ -10,6 +10,13 @@ eventFilter = cms.Sequence(preTrgTest *
                            hfCoincFilter *       ## E=3 GeV tower threshold
                            purityFractionFilter)
 
+eventFilterLoose = cms.Sequence(preTrgTest *
+                                minBiasBscFilter *    ## BSC OR, !BSCHALO
+                                postTrgTest *
+                                hfEitherOfTwoFilter *       ## E=3 GeV tower threshold
+                                purityFractionFilter)
+
+
 eventFilterMC = cms.Sequence(preTrgTest *
                              minBiasBscFilterMC *    ## BSC OR, !BSCHALO
                              postTrgTest *
