362200cc183e2009b572438106986056b2bead86
edwenger <>
2010-08-30 11:13:09 +0000

    settle upon raw data collector with getByLabel

--- Misc/SealModule.cc
+++ Misc/SealModule.cc
@@ -2,12 +2,10 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/ServiceRegistry/interface/ServiceMaker.h"
 #include "EventFilter/RawDataCollector/interface/RawDataCollectorModule.h"
-#include "EventFilter/RawDataCollector/interface/RawDataCollectorZSTracker.h"
 #include "EventFilter/RawDataCollector/interface/RawDataCollectorByLabel.h"
 
 using namespace edm::serviceregistry;
 
 DEFINE_FWK_MODULE(RawDataCollectorModule);
-DEFINE_FWK_MODULE(RawDataCollectorZSTracker);
 DEFINE_FWK_MODULE(RawDataCollectorByLabel);
 
--- Misc/rawDataCollectorZSTracker_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-rawDataCollector = cms.EDProducer("RawDataCollectorZSTracker",
-    currentProcessOnly = cms.bool(False),
-    preferLaterProcess = cms.bool(True),
-    verbose = cms.untracked.int32(1)     # 0 = quiet, 1 = collection list, 2 = FED list
-)
-
-
