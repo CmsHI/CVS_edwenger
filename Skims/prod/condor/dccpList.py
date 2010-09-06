eed7d4a581fd1d93a15ed20fe83977baba3b8452
frankma <>
2010-09-06 19:05:12 +0000

    update for job R10A-Jun14RR-*skim-v0

--- Skims/prod/condor/dccpList.py
+++ Skims/prod/condor/dccpList.py
@@ -1,21 +1,28 @@
 import os
 
 dcache  = "/pnfs/cmsaf.mit.edu/t2bat/cms"
-scratch = "/net/hidsk0001/d00/scratch/frankma/data/MinimumBias"
-#sample  = "MB-C10-M6RR-MBHfLooseskim-v0"
-sample  = "MB-C10-M6RR-JMTHfLooseskim-v0"
+scratch = "/net/hidsk0001/d00/scratch/frankma/data"
+samples  = {
+    "MinimumBias":"MB-R10A-Jun14RR-MBskim-v0",
+    "JetMETTau":"JMT-R10A-Jun14RR-J15Uskim-v0"
+    }
 
-filelist = []
+for PD,sample in samples.items():
+  outdir=scratch+"/"+PD+"/"+sample
+  os.system('mkdir -p '+outdir)
 
-for line in open ('pub_list/'+ sample + '.txt', 'r'):
+  filelist = []
+  for line in open ('pub_list/'+ sample + '.txt', 'r'):
     filelist.append(line.rstrip('\n').replace('trkAnaSkimAOD','trkhists'))
 
-for file in filelist:
+  for file in filelist:
+    infile=dcache+file
     subfile = file.split('/')
     #print 'preparing to copy ' + subfile[-1] + ' from dcache'
-    print 'preparing to copy ' + file + ' from dcache'
-    if not os.path.isfile(sample + "/" + subfile[-1]):
-        os.system('dccp ' + dcache + file + ' ' + scratch+"/"+sample)
+    if not os.path.isfile(outdir + "/" + subfile[-1]):
+      cmd = 'dccp ' + infile + ' ' + outdir
+      print cmd
+      os.system(cmd)
     else:
-        print subfile[-1] + " already exists in '" + scratch+"/"+sample + "' directory"
+      print subfile[-1] + " already exists in '" + outdir + "' directory"
 
