a87cce0bf52c984f821c1143f04cd53dc34f592d
frankma <>
2010-09-06 21:13:47 +0000

    test cfg on jmt-run10a-pr4, also test the new enableHLTJet function

--- Skims/test/TrkAnaSkim_data_7TeV_cfg.py
+++ Skims/test/TrkAnaSkim_data_7TeV_cfg.py
@@ -14,10 +14,14 @@ process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
       #'/store/data/Commissioning10/MinimumBias/RECO/Apr20ReReco-v1/0164/D237203A-DC4C-DF11-BBF5-0018F3D095FC.root'))
       #'/store/user/edwenger/MinimumBias/MB-C10-A20RR-TRKANASKIM-v4/38a0275e6a2a62980cf9b1868edbd493/trkAnaSkimAOD_97_1.root'))
-      # jun14rr
-      '/store/data/Run2010A/JetMETTau/RECO/Jun14thReReco_v2/0000/5C4D249B-4179-DF11-8CE3-0025B3E06396.root',
-      '/store/data/Run2010A/JetMETTau/RECO/Jun14thReReco_v2/0000/56D1F17A-3979-DF11-A340-003048D476DA.root',
-      '/store/data/Run2010A/JetMETTau/RECO/Jun14thReReco_v2/0000/4A5A42A3-4379-DF11-A264-003048636198.root'
+      # jun14rr: run 132440 - 137436
+      #'/store/data/Run2010A/JetMETTau/RECO/Jun14thReReco_v2/0000/5C4D249B-4179-DF11-8CE3-0025B3E06396.root',
+      #'/store/data/Run2010A/JetMETTau/RECO/Jun14thReReco_v2/0000/56D1F17A-3979-DF11-A340-003048D476DA.root',
+      #'/store/data/Run2010A/JetMETTau/RECO/Jun14thReReco_v2/0000/4A5A42A3-4379-DF11-A264-003048636198.root'
+      # jmt pr4: run >137436 (CMSSW_3_6_1_patch4) 
+      '/store/data/Run2010A/JetMETTau/RECO/v4/000/140/059/28ADC2FF-708E-DF11-B715-001D09F2527B.root',
+      '/store/data/Run2010A/JetMETTau/RECO/v4/000/140/059/24F181DC-8A8E-DF11-BCFE-003048F1183E.root',
+      '/store/data/Run2010A/JetMETTau/RECO/v4/000/140/059/22228061-A18E-DF11-9FAE-003048F1183E.root'
       ))
     # =============== Other Statements =====================
 
@@ -26,7 +30,7 @@ process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
 process.GlobalTag.globaltag = 'GR_R_36X_V12A::All'
 
 process.configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.16 $'),
+    version = cms.untracked.string('$Revision: 1.17 $'),
     name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/UserCode/edwenger/Skims/test/TrkAnaSkim_data_7TeV_cfg.py,v $'),
     annotation = cms.untracked.string('BPTX_AND + BSC_OR + !BSCHALO')
 )
@@ -54,7 +58,7 @@ removeMCMatching(process, ['All']) # turn off MC matching for data
 
 from edwenger.Skims.customise_cfi import *
 #enableEitherHFEvtSel(process) # Replace HF coinc with the looser any HF hit evt selection
-#enableHLTJet15U(process)
+enableHLTJet(process,"HLT_Jet50U")
 updateEvtSelEff(process.trackAna_STD,"STD_NSD_TrkVtx")
 updateEvtSelEff(process.looseTrackAna_STD,"STD_NSD_PixVtx")
 #process = enableAOD(process)
