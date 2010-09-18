3b7407e4803c21762cf8c140269e962f2ecdc34c
edwenger <>
2010-09-18 00:30:13 +0000

    towards incorporating in top-level event content

--- Tier0/EventFilter/Configuration/python/ZSDigiToRaw_EventContent_cff.py
+++ Tier0/EventFilter/Configuration/python/ZSDigiToRaw_EventContent_cff.py
@@ -1,21 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
-from Configuration.EventContent.EventContentHeavyIons_cff import RAWEventContent,RAWSIMEventContent
-
-OutZSRAW = cms.PSet(
-    SelectEvents = cms.untracked.PSet(
-      SelectEvents = cms.vstring()
-    ),
+ZSDigiToRawContent = cms.PSet(
     outputCommands = cms.untracked.vstring(
       'drop FEDRawDataCollection_*_*_*',
       'keep FEDRawDataCollection_source_*_RECO', 
       'keep FEDRawDataCollection_rawDataCollector_*_RECO',
       'keep *_*_APVCM_*')
     )
-
-import copy
-OutZSRAWData =copy.deepcopy(OutZSRAW)
-OutZSRAWData.outputCommands = RAWEventContent.outputCommands + OutZSRAWData.outputCommands
-
-OutZSRAWMC = copy.deepcopy(OutZSRAW)
-OutZSRAWMC.outputCommands = RAWSIMEventContent.outputCommands + OutZSRAWMC.outputCommands
