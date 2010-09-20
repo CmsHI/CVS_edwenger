b1959b1f28bb4fa35a429129e4fad881866a0f8d
edwenger <>
2010-09-20 10:52:08 +0000

    add HcalNZS sequence

---
+++ Tier0/Configuration/StandardSequences/python/ReconstructionHeavyIons_cff.py
@@ -0,0 +1,42 @@
+import FWCore.ParameterSet.Config as cms
+
+#--------------------------------------------------------------------------
+# LOCAL RECO
+
+# Tracker
+from RecoVertex.BeamSpotProducer.BeamSpot_cfi import *
+from RecoLocalTracker.Configuration.RecoLocalTracker_cff import *
+
+# Ecal
+from RecoLocalCalo.Configuration.ecalLocalRecoSequence_cff import *
+
+# Hcal
+from RecoLocalCalo.Configuration.hcalLocalReco_cff import *
+from RecoLocalCalo.Configuration.hcalLocalRecoNZS_cff import *
+
+# Muons
+from RecoLocalMuon.Configuration.RecoLocalMuon_cff import *
+from RecoLuminosity.LumiProducer.lumiProducer_cff import *
+
+#--------------------------------------------------------------------------
+# HIGH LEVEL RECO
+
+from RecoHI.Configuration.Reconstruction_HI_cff import *
+
+#--------------------------------------------------------------------------
+
+caloReco = cms.Sequence(ecalLocalRecoSequence*hcalLocalRecoSequence)
+caloRecoNZS = cms.Sequence(caloReco+hcalLocalRecoSequenceNZS)
+muonReco = cms.Sequence(trackerlocalreco+muonlocalreco+lumiProducer)
+
+localReco = cms.Sequence(offlineBeamSpot*muonReco*caloReco)
+localReco_HcalNZS = cms.Sequence(offlineBeamSpot*muonReco*caloRecoNZS)
+
+#--------------------------------------------------------------------------
+# Main Sequence
+
+reconstruct_PbPb = cms.Sequence(localReco*globalRecoPbPb)
+reconstructionHeavyIons = cms.Sequence(reconstruct_PbPb)
+reconstructionHeavyIons_HcalNZS = cms.Sequence(localReco_HcalNZS*globalRecoPbPb)
+
+#--------------------------------------------------------------------------
