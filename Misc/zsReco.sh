64ebf0436d1a855399ee69eb2be7b75764dbec78
edwenger <>
2010-09-23 10:46:36 +0000

    add '--zs' flag

--- Misc/zsReco.sh
+++ Misc/zsReco.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-infile="file:dijet80_VR_RECO_inZSRAW.root"
+infile="file:dijet80_VR_RECO_inZSRAWSIM.root"
 outfile="dijet80_ZS_RECO.root"
 
 cmsDriver.py zsReco -n 1 \
 -s RAW2DIGI,RECO  --scenario HeavyIons --himix \
---conditions auto:mc \
+--conditions auto:mc --zs \
 --datatier 'GEN-SIM-RECO' --eventcontent=RECOSIM \
 --filein=$infile --fileout=$outfile --processName 'zsRECO' \
 --customise edwenger/Misc/customiseStripDB.py \
