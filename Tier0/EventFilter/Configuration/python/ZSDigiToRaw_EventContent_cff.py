0d986619eb6caa3c7434696ffd8eed3f1db4174b
edwenger <>
2010-09-23 12:01:11 +0000

    don't need to specify process name now that label is uniquely defined

--- Tier0/EventFilter/Configuration/python/ZSDigiToRaw_EventContent_cff.py
+++ Tier0/EventFilter/Configuration/python/ZSDigiToRaw_EventContent_cff.py
@@ -3,6 +3,6 @@ import FWCore.ParameterSet.Config as cms
 ZSDigiToRawContent = cms.PSet(
     outputCommands = cms.untracked.vstring(
       'drop FEDRawDataCollection_*_*_*',
-      'keep FEDRawDataCollection_zsSource_*_RECO'
+      'keep FEDRawDataCollection_zsSource_*_*'
       )
     )
