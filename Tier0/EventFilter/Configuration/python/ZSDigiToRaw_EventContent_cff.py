c8e6c06a13c5fc1345a1b3024aeeb35ce135011a
edwenger <>
2010-09-23 19:48:19 +0000

    switch to REPACK naming

--- Tier0/EventFilter/Configuration/python/ZSDigiToRaw_EventContent_cff.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-ZSDigiToRawContent = cms.PSet(
-    outputCommands = cms.untracked.vstring(
-      'drop FEDRawDataCollection_*_*_*',
-      'keep FEDRawDataCollection_zsSource_*_*'
-      )
-    )
