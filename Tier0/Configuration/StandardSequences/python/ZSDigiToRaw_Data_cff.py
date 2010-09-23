72554b05d792cf0336da0e5dcd1268b262ff5a0b
edwenger <>
2010-09-23 19:47:23 +0000

    switch from ZSDIGI2RAW to REPACK

--- Tier0/Configuration/StandardSequences/python/ZSDigiToRaw_Data_cff.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-# Virgin Raw + ZS SiStripDigis --> ZS Raw sequence
-from EventFilter.Configuration.ZSDigiToRaw_cff import *
-
-ZSDigiToRaw = cms.Sequence(zsDigiToRaw)
