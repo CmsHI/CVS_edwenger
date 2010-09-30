d2ff759946ce1e37e843dc50366004336ba7c9b9
edwenger <>
2010-09-30 03:26:38 +0000

    testing partial suppression

--- Misc/vrRecoData.sh
+++ Misc/vrRecoData.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 infile="/store/express/Run2010B/ExpressPhysics/FEVT/Express-v2/000/146/417/10F981FF-5EC6-DF11-9657-0030486733B4.root"
-outfile="data_VR_RECO.root"
+outfile="data_vrRECO.root"
 
 cmsDriver.py vrRecoData -n -1 \
--s RAW2DIGI,RECO,REPACK  --scenario HeavyIons \
+-s RAW2DIGI,RECO,REPACK:DigiToSplitRawRepack  --scenario HeavyIons \
 --conditions FrontierConditions_GlobalTag,GR_R_39X_V0::All --data \
 --datatier 'RECO','REPACK' --eventcontent RECO,REPACKRAW \
---filein=$infile --fileout=$outfile --processName 'RERECO' \
+--filein=$infile --fileout=$outfile --processName 'vrRECO' \
+--customise RecoLocalTracker/SiStripZeroSuppression/customiseCMN.py \
+--cust_function customisePartialSuppress \
 --no_exec
--- Misc/zsRecoData.sh
+++ Misc/zsRecoData.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-infile="file:data_VR_RECO_inREPACKRAW.root"
-outfile="data_ZS_RECO.root"
+infile="file:data_vrRECO_inREPACKRAW.root"
+outfile="data_zsRECO.root"
 
 cmsDriver.py zsRecoData -n -1 \
--s RAW2DIGI,RECO --scenario HeavyIons --repacked \
+-s RAW2DIGI:RawToDigi_withVR,RECO --scenario HeavyIons --repacked \
 --conditions FrontierConditions_GlobalTag,GR_R_39X_V0::All --data \
 --datatier 'RECO' --eventcontent=RECO \
 --filein=$infile --fileout=$outfile --processName 'zsRECO' \
+--customise RecoLocalTracker/SiStripZeroSuppression/customiseCMN.py \
+--cust_function customiseMergeCollections \
 --no_exec
