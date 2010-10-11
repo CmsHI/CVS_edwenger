6296f5bec6fdb17964510350fe46168fb5862107
edwenger <>
2010-10-11 21:22:18 +0000

    refine sequences

--- DataMixingModule/recoDM.sh
+++ DataMixingModule/recoDM.sh
@@ -3,7 +3,7 @@
 infile="file:SingleZmumu_MatchVertexDM_DIGI2RAW.root"
 outfile="hiRecoDM_RECO.root"
 
-cmsDriver.py hiRecoDM -n 1 --scenario HeavyIons \
+cmsDriver.py hiRecoDM -n 10 --scenario HeavyIons \
 -s RAW2DIGI,L1Reco,RECO --processName TEST \
 --datatier GEN-SIM-RECO --eventcontent FEVTDEBUG \
 --customise SimGeneral.DataMixingModule.customiseMatchRecVertex,SimGeneral.DataMixingModule.DataMixer_DataConditions_3_8_X_data2010 \
