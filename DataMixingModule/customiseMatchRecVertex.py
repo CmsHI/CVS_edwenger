e782e66828a3b0bbd7eed5de799205fc53eb5011
edwenger <>
2010-10-11 21:06:28 +0000

    simplify customization

--- DataMixingModule/customiseMatchRecVertex.py
+++ DataMixingModule/customiseMatchRecVertex.py
@@ -1,42 +1,44 @@
 import FWCore.ParameterSet.Config as cms
 
 ##############################################################################
-def customiseDataMixerOutput(process):
 
+def customiseDataMixerOutput(process):
     process.DATAMIXERoutput.outputCommands.extend(
         ['keep *_offlinePrimaryVertices_*_*',
          'keep *_siStripDigis_VirginRaw_*'])
-
     return process
 
 ##############################################################################
-def customiseMatchRecVertex(process):
 
+def customiseCloneGenerator(process):
     process.generator = process.hiSignal.clone(embeddingMode=cms.bool(False))
+    return process
+
+def customiseMatchRecVertex(process):
     process.load("SimGeneral.MixingModule.MatchRecVtx_cfi")
     process.pgen.replace(process.VertexSmearing,process.matchRecVtx)
     process.mixData.input.sequential = True
-
     return process
 
-##############################################################################
 def customiseSiStripRawDigi(process):
-
     process.mixData.SiStripRawDigiBkgd = True # use VirginRaw background digis
     process.SiStripDigiToRaw.FedReadoutMode = cms.string('VIRGIN_RAW') # pack rawdigis as VR
+    return process
 
+def customiseCloneMatchRaw(process):
+    customiseCloneGenerator(process)
+    customiseMatchRecVertex(process)
+    customiseSiStripRawDigi(process)
     return process
 
 ##############################################################################
-def customiseBeamSpot(process):
 
+def customiseBeamSpot(process):
     # Loosen beamspot-dependent cuts
     # Probably better to do ESPrefer of custom offlineBeamSpot object
-    
-    #process.hiPixel3ProtoTracks.RegionFactoryPSet.RegionPSet.useFoundVertices = False
     process.hiSelectedProtoTracks.maxD0Significance = 1000.0
     process.hiSelectedProtoTracks.minZCut = 0.5
     process.hiSelectedTracks.d0_par2 = [300.0,0.3]
     process.hiSelectedTracks.dz_par2 = [300.0,0.3]
-
+    #process.hiPixel3ProtoTracks.RegionFactoryPSet.RegionPSet.useFoundVertices = False
     return process
--- DataMixingModule/simOnRecoDM.sh
+++ DataMixingModule/simOnRecoDM.sh
@@ -7,6 +7,6 @@ cmsDriver.py Configuration/GenProduction/SingleZmumu_FlatPt0to20_cfi -n 10 \
 -s GEN,SIM,DIGI,DATAMIX,L1,DIGI2RAW \
 --conditions FrontierConditions_GlobalTag,START38_V8::All \
 --eventcontent RAWDEBUG \
---customise SimGeneral.DataMixingModule.customiseMatchRecVertex,SimGeneral.DataMixingModule.customiseMatchRecVertex,SimGeneral.DataMixingModule.DataMixer_DataConditions_3_8_X_data2010 \
---cust_function customiseMatchRecVertex,customiseSiStripRawDigi,customise \
+--customise SimGeneral.DataMixingModule.customiseMatchRecVertex,SimGeneral.DataMixingModule.DataMixer_DataConditions_3_8_X_data2010 \
+--cust_function customiseCloneMatchRaw,customise \
 --filein $infile --fileout $outfile --no_exec
