7921abfca256cfb4a917956dec80a22b2aee5cac
frankma <>
2010-10-18 08:16:53 +0000

    proc1014: on jm-r10a-pr4 15U, 50U, with updated hist ranges for trackAna, and djana

--- Skims/prod/condor/submitAll.py
+++ Skims/prod/condor/submitAll.py
@@ -2,15 +2,16 @@ import os
 os.system("./create_custom_python_cfg.sh")
 
 scratch="/net/hidsk0001/d00/scratch/frankma"
-jobTag="proc1001"
+jobTag="proc1014"
 
 # ================== 362 ===============
 samples  = {
     "JM-R10A-PR4-Jet50Uskim-v0":"JetMETTau"
+    "JM-R10A-PR4-Jet15Uskim-v0":"JetMETTau"
     }
 
 for sample,PD in samples.items():
-  inputList="pub_list/"+sample+".txt"
+  inputList="~/inputs_data/"+sample+".txt"
   outputDir=scratch+"/data/"+PD+"/"+sample+"_"+jobTag
   cmd="./submitJob.sh "+inputList+" "+outputDir
   print cmd
