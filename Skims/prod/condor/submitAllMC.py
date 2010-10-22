e415b30ba4cb77530d83b18c1d6e26bb02b7022e
frankma <>
2010-10-22 09:48:17 +0000

    proc1022_final: light output

--- Skims/prod/condor/submitAllMC.py
+++ Skims/prod/condor/submitAllMC.py
@@ -2,11 +2,11 @@ import os
 os.system("./create_custom_python_cfg.sh")
 
 scratch="/net/hidsk0001/d00/scratch/frankma"
-jobTag="proc1022_final"
+jobTag="proc1022_final_LIGHT"
 
 # ================== 362 ===============
 samples  = {
-#    "su10-qcdflat-startup36v9":"QCD"
+    "su10-qcdflat-startup36v9":"QCD",
     "su10-qcd80-startup36v9_f500":"QCD"
     }
 
