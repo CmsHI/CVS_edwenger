e84a10280b55612838c3df85df19202cd252c786
frankma <>
2010-10-27 17:36:31 +0000

    proc1022_final_v2: save FF

--- Skims/prod/condor/submitAll.py
+++ Skims/prod/condor/submitAll.py
@@ -2,7 +2,7 @@ import os
 os.system("./create_custom_python_cfg.sh")
 
 scratch="/net/hidsk0001/d00/scratch/frankma"
-jobTag="proc1022_final_LIGHT"
+jobTag="proc1022_final_v2"
 
 # ================== 362 ===============
 samples  = {
