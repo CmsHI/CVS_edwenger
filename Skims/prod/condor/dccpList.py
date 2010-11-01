c85baeb5cbe560674f78d80d175eb3a12fd0658a
frankma <>
2010-11-01 21:06:59 +0000

    copied 900 data skims in 362

--- Skims/prod/condor/dccpList.py
+++ Skims/prod/condor/dccpList.py
@@ -2,11 +2,14 @@ import os
 
 dcache  = "/pnfs/cmsaf.mit.edu/t2bat/cms"
 scratch = "/net/hidsk0001/d00/scratch/frankma/data"
+inputs_dir = '/net/hisrv0001/home/frankma/inputs_data'
 samples  = {
+    "MB-C10-Jun14RR-900GeV-MBskim-v1":"MinimumBias",
+    "MB-C10-Jun14RR-900GeV-JMTskim-v1":"JetMETTau"
     #"MB-R10A-Jun14RR-MBskim-v0":"MinimumBias",
     #"JMT-R10A-Jun14RR-J15Uskim-v0":"JetMETTau"
-    "JMT-R10A-PR4-Jet50Uskim-v0":"JetMETTau",
-    "JMT-R10A-PR4-Jet15Uskim-v0":"JetMETTau"
+    #"JMT-R10A-PR4-Jet50Uskim-v0":"JetMETTau",
+    #"JMT-R10A-PR4-Jet15Uskim-v0":"JetMETTau"
     }
 
 for sample,PD in samples.items():
@@ -15,7 +18,7 @@ for sample,PD in samples.items():
   os.system('mkdir -p '+outdir)
 
   filelist = []
-  for line in open ('pub_list/'+ sample + '.txt', 'r'):
+  for line in open (inputs_dir+"/"+ sample + '.txt', 'r'):
     filelist.append(line.rstrip('\n').replace('trkAnaSkimAOD','trkhists'))
 
   for file in filelist:
