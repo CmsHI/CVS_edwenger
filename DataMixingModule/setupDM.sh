5abf6a9769dadf4cdbb7cb082e9d9ea6ff7574b7
edwenger <>
2010-11-20 12:18:12 +0000

    standard sequences for HF tech triggers

--- DataMixingModule/setupDM.sh
+++ DataMixingModule/setupDM.sh
@@ -7,6 +7,7 @@ dir2=SimGeneral/DataMixingModule
 addpkg $dir2
 cvs co Configuration/GenProduction
 cvs co -r V05-02-11 SimGeneral/MixingModule
+addpkg Configuration/StandardSequences
 
 # HIP monitoring
 # cvs co -d TkDPG UserCode/giordano/TkDPG
@@ -14,4 +15,6 @@ cvs co -r V05-02-11 SimGeneral/MixingModule
 cd $dir
 cp *.h *.cc $CMSSW_BASE/src/$dir2/plugins
 cp customise*.py *cfi.py $CMSSW_BASE/src/$dir2/python
+cp *cff.py $CMSSW_BASE/src/Configuration/StandardSequences/python
 cp *.sh $CMSSW_BASE/src/$dir2/test
+
