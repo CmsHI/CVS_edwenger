1f386499b1e0772c04b418af37f8c92d29b75775
edwenger <>
2010-11-20 02:51:37 +0000

    update global tag

--- DataMixingModule/simOnRecoDM.sh
+++ DataMixingModule/simOnRecoDM.sh
@@ -5,8 +5,7 @@ outfile="SingleZmumu_MatchVertexDM_DIGI2RAW.root"
 
 cmsDriver.py Configuration/GenProduction/SingleZmumu_FlatPt0to20_cfi -n 10 \
 -s GEN,SIM,DIGI,DATAMIX,L1,DIGI2RAW \
---conditions FrontierConditions_GlobalTag,START38_V8::All \
---eventcontent RAWDEBUG \
+--conditions START39_V5HI::All --eventcontent RAWDEBUG \
 --customise SimGeneral.DataMixingModule.customiseMatchRecVertex,SimGeneral.DataMixingModule.DataMixer_DataConditions_3_8_X_data2010 \
 --cust_function customiseCloneMatchRaw,customise \
 --filein $infile --fileout $outfile --no_exec
