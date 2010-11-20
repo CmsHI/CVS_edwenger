d0cd3cc224a8f89caac4d519ce18faa2c80299a4
edwenger <>
2010-11-20 12:57:54 +0000

    TLR to be the same as prompt/express reco

--- DataMixingModule/recoDM.sh
+++ DataMixingModule/recoDM.sh
@@ -6,7 +6,7 @@ outfile="hiRecoDM_RECO.root"
 cmsDriver.py hiRecoDM -n 10 --scenario HeavyIons \
 -s RAW2DIGI,L1Reco,RECO --processName TEST \
 --datatier GEN-SIM-RECO --eventcontent FEVTDEBUG \
---customise SimGeneral.DataMixingModule.customiseMatchRecVertex,SimGeneral.DataMixingModule.DataMixer_DataConditions_3_8_X_data2010 \
---cust_function customiseBeamRaw,customise \
+--customise SimGeneral.DataMixingModule.customiseMatchRecVertex,SimGeneral.DataMixingModule.DataMixer_DataConditions_3_8_X_data2010,Configuration.GlobalRuns.reco_TLR_39X \
+--cust_function customiseBeamRaw,customise,customiseCommonHI \
 --geometry DB --filein $infile --fileout $outfile \
 --conditions START39_V5HI::All --no_exec
