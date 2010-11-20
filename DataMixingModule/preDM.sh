acd55ded383d56839389571d137886fe8364a94e
edwenger <>
2010-11-20 02:25:40 +0000

    use dbsquery to get parent file

--- DataMixingModule/preDM.sh
+++ DataMixingModule/preDM.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-infile="/store/hidata/HIRun2010/HICorePhysics/RECO/PromptReco-v3/000/151/353/DC04F2F3-32F2-DF11-BD72-0030487C5CE2.root"
-secfile="/store/hidata/HIRun2010/HICorePhysics/RAW/v1/000/151/353/00B95950-00F2-DF11-9485-001D09F252DA.root"
-
 cmsDriver.py DMPreProcess -n 10 \
 -s RAW2DIGI --eventcontent DATAMIXER \
---conditions FrontierConditions_GlobalTag,GR10_P_V12::All \
---filein $infile --secondfilein $secfile --data \
+--conditions FrontierConditions_GlobalTag,GR10_P_V12::All --data \
+--dbsquery "find file,file.parent where run=151353 and lumi=100 and dataset like *HICorePhysics*/RECO" \
 --customise SimGeneral.DataMixingModule.customiseMatchRecVertex \
 --cust_function customiseDataMixerOutput \
 --no_exec
