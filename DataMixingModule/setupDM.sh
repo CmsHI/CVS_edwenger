fd20b5c235c0bf0b6e08e258ce2251f3085525a8
edwenger <>
2010-11-22 00:28:15 +0000

    update setup script for GenToRecoVtx

--- DataMixingModule/setupDM.sh
+++ DataMixingModule/setupDM.sh
@@ -13,7 +13,8 @@ addpkg Configuration/StandardSequences
 # cvs co -d TkDPG UserCode/giordano/TkDPG
 
 cd $dir
-cp *.h *.cc $CMSSW_BASE/src/$dir2/plugins
+cp BuildFile.xml Gen*.cc $CMSSW_BASE/src/SimGeneral/MixingModule/plugins
+cp Data*.h Data*.cc $CMSSW_BASE/src/$dir2/plugins
 cp customise*.py *cfi.py $CMSSW_BASE/src/$dir2/python
 cp *cff.py $CMSSW_BASE/src/Configuration/StandardSequences/python
 cp *.sh $CMSSW_BASE/src/$dir2/test
