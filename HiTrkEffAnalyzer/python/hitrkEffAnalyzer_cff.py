ea1fdde71a3cd8fd769a682a897235289927dd17
sungho <>
2010-11-22 12:37:14 +0000

    usejet set to false in cfi

--- HiTrkEffAnalyzer/python/hitrkEffAnalyzer_cff.py
+++ HiTrkEffAnalyzer/python/hitrkEffAnalyzer_cff.py
@@ -13,7 +13,7 @@ hitrkEffAnalyzer.label_tp_effic = cms.untracked.InputTag("cutsTPForEff")
 hitrkEffAnalyzer.label_tp_fake = cms.untracked.InputTag("cutsTPForFak")
 hitrkEffAnalyzer.hasSimInfo = cms.untracked.bool(True) # without this no sim track info
 hitrkEffAnalyzer.doAssociation = cms.untracked.bool(True)
-hitrkEffAnalyzer.useJetEt = cms.untracked.bool(False)
+
 
 hitrkEffAna = cms.Sequence(cutsTPForFak*
                            cutsTPForEff*
