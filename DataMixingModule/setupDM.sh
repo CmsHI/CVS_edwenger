6c23849509c90ae4437446cd6e17ef6a043859f9
edwenger <>
2010-11-24 20:53:33 +0000

    fewer stuff from usercode needed in 394

--- DataMixingModule/setupDM.sh
+++ DataMixingModule/setupDM.sh
@@ -4,18 +4,14 @@ dir=`pwd`
 
 cd $CMSSW_BASE/src
 dir2=SimGeneral/DataMixingModule
-addpkg $dir2
+cvs co -r V00-02-20 $dir2
 cvs co Configuration/GenProduction
-cvs co -r V05-02-11 SimGeneral/MixingModule
-addpkg Configuration/StandardSequences
+addpkg SimGeneral/MixingModule
 
 # HIP monitoring
 # cvs co -d TkDPG UserCode/giordano/TkDPG
 
 cd $dir
 cp BuildFile.xml Gen*.cc $CMSSW_BASE/src/SimGeneral/MixingModule/plugins
-cp Data*.h Data*.cc $CMSSW_BASE/src/$dir2/plugins
-cp customise*.py *cfi.py $CMSSW_BASE/src/$dir2/python
-cp *cff.py $CMSSW_BASE/src/Configuration/StandardSequences/python
-cp *.sh $CMSSW_BASE/src/$dir2/test
+cp customise*.py $CMSSW_BASE/src/$dir2/python
 
