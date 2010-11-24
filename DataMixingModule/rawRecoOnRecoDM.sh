a49d698ced488171eadb0cc2b357e3b1fb8f7f5b
edwenger <>
2010-11-24 23:36:23 +0000

    for embedding p+p data signal into HI data background

---
+++ DataMixingModule/rawRecoOnRecoDM.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+infile="/store/data/Run2010B/Photon/RAW-RECO/v2/000/146/644/B8A9A7E7-11CB-DF11-8BAF-1CC1DE04FF98.root"
+outfile="DMRawRecoOnReco_DIGI2RAW.root"
+
+cmsDriver.py DMRawRecoOnReco -n 10 \
+-s RAW2DIGI,DATAMIX,L1,DIGI2RAW,HLT:HIon \
+--datamix DataOnData --data \
+--conditions GR_R_38X_V15::All \
+--eventcontent RAW \
+--customise SimGeneral.DataMixingModule.customiseMatchRecVertex \
+--cust_function customiseSiStripRawDigi \
+--filein $infile --fileout $outfile --no_exec
