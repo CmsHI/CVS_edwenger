526e6ad3db4e799171b83ed518afd1be4b486a1a
edwenger <>
2010-11-25 10:21:45 +0000

    even though it is data, we need to do RAW2DIGI on 'rawdatacollector'

--- DataMixingModule/dataRecoDM.sh
+++ DataMixingModule/dataRecoDM.sh
@@ -4,7 +4,7 @@ infile="file:DMRawRecoOnReco_DIGI2RAW.root"
 outfile="hiRecoDM_RECO.root"
 
 cmsDriver.py hiRecoDM -n 10 --scenario HeavyIons \
--s RAW2DIGI,L1Reco,RECO --processName TEST --data \
+-s RAW2DIGI,L1Reco,RECO --processName TEST \
 --datatier GEN-SIM-RECO --eventcontent RECO \
 --customise SimGeneral.DataMixingModule.customiseMatchRecVertex,Configuration.GlobalRuns.reco_TLR_39X \
 --cust_function customiseVtxPP,customiseCommonHI \
