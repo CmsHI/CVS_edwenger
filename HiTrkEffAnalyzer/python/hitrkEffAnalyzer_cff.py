416a8c28b9a917d47930e61a640366746e72901b
sungho <>
2010-12-21 19:10:45 +0000

    turn off pixel mult mode to use Jet ET (not necessarily as occ handle but as handle for merging MB + HLT Jet)

--- HiTrkEffAnalyzer/python/hitrkEffAnalyzer_cff.py
+++ HiTrkEffAnalyzer/python/hitrkEffAnalyzer_cff.py
@@ -13,7 +13,7 @@ hitrkEffAnalyzer.label_tp_effic = cms.untracked.InputTag("cutsTPForEff")
 hitrkEffAnalyzer.label_tp_fake = cms.untracked.InputTag("cutsTPForFak")
 hitrkEffAnalyzer.hasSimInfo = cms.untracked.bool(True) # without this no sim track info
 hitrkEffAnalyzer.doAssociation = cms.untracked.bool(True)
-hitrkEffAnalyzer.pixelMultMode =  cms.untracked.bool(True)
+hitrkEffAnalyzer.pixelMultMode =  cms.untracked.bool(False)
 hitrkEffAnalyzer.useJetEt = cms.untracked.bool(True)
 
 hitrkEffAna = cms.Sequence(cutsTPForFak*
