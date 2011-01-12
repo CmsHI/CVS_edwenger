4ee261e5ca77bbbf1070aa94e9c1816340b5526f
frankma <>
2011-01-12 10:10:05 +0000

    proc0111_JetVtx

--- Skims/prod/condor/cleanAllJobs.py
+++ Skims/prod/condor/cleanAllJobs.py
@@ -1,11 +1,15 @@
 import os
 
 scratch="/net/hidsk0001/d00/scratch/frankma"
-jobTag="proc1001"
+jobTag="proc0111_JetVtx"
 
 # ================== 362 ===============
 samples  = {
-    "JM-R10A-PR4-Jet50Uskim-v0":"JetMETTau"
+    #"MB-C10-M6RR-JMTskim-v0":"MinimumBias",
+    #"MB-C10-PR9-JMTskim-v0":"MinimumBias",
+    #"JMT-R10A-PR1-JMTskim-v0":"JetMETTau",
+    #"JM-R10A-PR4-Jet50Uskim-v0":"JetMETTau",
+    "JM-R10A-PR4-Jet15Uskim-v0":"JetMETTau"
     }
 
 for sample,PD in samples.items():
--- Skims/prod/condor/mergeAllJobs.py
+++ Skims/prod/condor/mergeAllJobs.py
@@ -1,14 +1,10 @@
 import os
 
 scratch="/net/hidsk0001/d00/scratch/frankma/data"
-#jobTag="proc1022_final_v2"
+jobTag="proc0111_JetVtx"
 
-# ================== 362 ===============
-samples  = {
-    #"MB-C10-Jun14RR-900GeV-MBskim-v1":"MinimumBias",
-    #"MB-C10-Jun14RR-900GeV-JMTskim-v1":"JetMETTau",
-    #"JM-R10A-PR4-Jet50Uskim-v0":"JetMETTau",
-    #"JM-R10A-PR4-Jet15Uskim-v0":"JetMETTau"
+# ================== 39X ===============
+hisamples  = {
     "hcpr-spectra-v2-all":"HCPR",
     "hcpr-spectra-v2-0to5":"HCPR",
     "hcpr-spectra-v2-5to10":"HCPR",
@@ -17,11 +13,24 @@ samples  = {
     "hcpr-spectra-v2-50to100":"HCPR"
     }
 
+# ================== 362 ===============
+ppsamples  = {
+    # 900GeV
+    #"MB-C10-Jun14RR-900GeV-MBskim-v1":"MinimumBias",
+    #"MB-C10-Jun14RR-900GeV-JMTskim-v1":"JetMETTau",
+    # 7TeV
+    "MB-C10-M6RR-JMTskim-v0":"MinimumBias",
+    "MB-C10-PR9-JMTskim-v0":"MinimumBias",
+    "JMT-R10A-PR1-JMTskim-v0":"JetMETTau",
+    "JM-R10A-PR4-Jet50Uskim-v0":"JetMETTau",
+    "JM-R10A-PR4-Jet15Uskim-v0":"JetMETTau"
+    }
+
 inFolder="."
 outFolder="mergeAll"
-for sample,PD in samples.items():
-  #indir=scratch+"/"+PD+"/"+sample+"_"+jobTag
-  indir=scratch+"/"+PD+"/"+sample
+for sample,PD in ppsamples.items():
+  indir=scratch+"/"+PD+"/"+sample+"_"+jobTag
+  #indir=scratch+"/"+PD+"/"+sample
   cmd="./mergeJob.sh " + indir+"/"+inFolder + " trkhists " + indir+"/"+outFolder
   print cmd
   #os.system(cmd)
