3f471e76845955d1bc5fe240e0e2fa1fd36ed930
sungho <>
2011-03-15 20:33:58 +0000

    no trigger by default

--- HiEvtSelAnalyzer/python/hievtselanalyzer_cfi.py
+++ HiEvtSelAnalyzer/python/hievtselanalyzer_cfi.py
@@ -8,9 +8,5 @@ hievtselanalyzer = cms.EDAnalyzer('HiEvtSelAnalyzer',
        isGEN=cms.untracked.bool(False),
        ptMin=cms.untracked.double(0.9),
        neededCentBins = cms.untracked.vint32(0,1,3,11,19,39), # 0~5% (0,1), 5~10% (2,3), 10~30% (4,11) etc..
-       trignames=cms.untracked.vstring('HLT_HIMinBiasHF',
-                                       'HLT_HIJet35U',
-                                       'HLT_HIJet50U',
-                                       'HLT_HIJet75U',
-                                       'HLT_HIJet90U')
+       trignames=cms.untracked.vstring() # no triggers
 )
