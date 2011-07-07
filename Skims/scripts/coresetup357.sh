0a972e81328f914d1de686263f3914a2d749e8df
sungho <>
2011-07-07 21:11:29 +0000

    updated setup

--- Skims/scripts/coresetup357.sh
+++ Skims/scripts/coresetup357.sh
@@ -4,13 +4,14 @@ cd $CMSSW_BASE/src
 
 # skiming code
 cvs co UserCode/edwenger/Skims
+cvs co -r 1.4 UserCode/edwenger/Skims/python/patAna_cff.py
 
 # analyzers
 cvs co UserCode/edwenger/EvtSelAnalyzer
 cvs co UserCode/edwenger/VertexAnalyzer
 cvs co UserCode/edwenger/TrackSpectraAnalyzer
 cvs co UserCode/edwenger/TrkEffAnalyzer
-cvs co -d Saved UserCode/SavedFMa/Saved
+#cvs co -d Saved UserCode/SavedFMa/Saved
 
 # agglomerative vertex
 cvs co -r V00-00-07 UserCode/FerencSiklerVertexing
@@ -22,3 +23,5 @@ cvs co UserCode/ASYoon/PPTrackingTools/VertexConstraintProducer
 mv UserCode/edwenger .
 mv UserCode/ASYoon/PPTrackingTools/ .
 rm -r UserCode/ASYoon
+
+scram b
